8f26ab47e90782ac261c56e207cd352e1a2ed66b
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-04 17:06:08 +0900

    MemberRepository with MemberRepositoryTest

--- src/main/java/community/independe/repository/MemberRepository.java
+++ src/main/java/community/independe/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import community.independe.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByUsername(String username); // 사용자 id 찾기
+
 }
--- src/test/java/community/independe/repository/MemberRepositoryTest.java
+++ src/test/java/community/independe/repository/MemberRepositoryTest.java
@@ -1,13 +1,52 @@
 package community.independe.repository;
 
+import community.independe.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
 class MemberRepositoryTest {
 
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @Test
+    public void basicMemberTest() {
+
+        Member member1 = Member.builder()
+                .username("id1")
+                .password("1234")
+                .nickname("nick1")
+                .role("ROLE_USER")
+                .build();
+
+        Member member2 = Member.builder()
+                .username("id2")
+                .password("1234")
+                .nickname("nick2")
+                .role("ROLE_USER")
+                .build();
+
+        Member savedMember1 = memberRepository.save(member1);
+        Member savedMember2 = memberRepository.save(member2);
+
+        Member findMember = memberRepository.findByUsername(savedMember1.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(savedMember1);
+
+        List<Member> findMembers = memberRepository.findAll();
+        Assertions.assertThat(findMembers.size()).isEqualTo(2);
+
+        memberRepository.delete(savedMember2);
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(1);
+
+    }
 }
