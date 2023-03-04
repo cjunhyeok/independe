ad9ac204cedf9f1a3f5ddc57593ae265e492605e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-04 16:50:37 +0900

    refactor MemberRepositoryTest to MemberTest

--- src/test/java/community/independe/repository/MemberRepositoryTest.java
+++ src/test/java/community/independe/repository/MemberRepositoryTest.java
@@ -1,9 +1,5 @@
 package community.independe.repository;
 
-import community.independe.domain.member.Member;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,44 +9,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Rollback(value = false)
 class MemberRepositoryTest {
 
-    @Autowired
-    private MemberRepository memberRepository;
 
-    @Test
-    public void testMember() {
-        //given
-        Member member = Member.builder()
-                .username("member1")
-                .password("123")
-                .nickname("nick1")
-                .role("ROLE_USER")
-                .build();
-
-        //when
-        Member savedMember = memberRepository.save(member);
-        Member findMember = memberRepository.findById(savedMember.getId())
-                .orElseThrow(() -> new IllegalArgumentException("user not exist"));
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-    }
-
-    @Test
-    public void noMember() {
-        //given
-        Member member = Member.builder()
-                .username("member1")
-                .password("123")
-                .nickname("nick1")
-                .role("ROLE_USER")
-                .build();
-
-        //when
-        Member savedMember = memberRepository.save(member);
-
-        //then
-        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class,
-                () -> memberRepository.findById(2L)
-                        .orElseThrow(() -> new IllegalArgumentException()));
-    }
 }
