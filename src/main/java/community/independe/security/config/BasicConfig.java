e1f2676727e3db397febf9cd5a92b8be40547f52
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-05 16:08:03 +0900

    add passwordEncoder

--- src/main/java/community/independe/security/config/BasicConfig.java
+++ src/main/java/community/independe/security/config/BasicConfig.java
@@ -1,13 +1,17 @@
 package community.independe.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class BasicConfig {
 
     @Bean
@@ -17,5 +21,10 @@ public class BasicConfig {
         http.formLogin();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
