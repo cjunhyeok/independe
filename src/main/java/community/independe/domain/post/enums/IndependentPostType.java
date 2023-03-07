eac28f8269463e5612ed7bc9263639add4c9611d
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-07 21:03:40 +0900

    refactor enum for thymeleaf

--- src/main/java/community/independe/domain/post/enums/IndependentPostType.java
+++ src/main/java/community/independe/domain/post/enums/IndependentPostType.java
@@ -1,5 +1,16 @@
 package community.independe.domain.post.enums;
 
 public enum IndependentPostType {
-    CLEAN, WASH, COOK, HEALTH, SERVICE
+
+    CLEAN("청소"), WASH("빨레"), COOK("요리"), HEALTH("건강"), ETC("기타");
+
+    private final String description;
+
+    IndependentPostType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
