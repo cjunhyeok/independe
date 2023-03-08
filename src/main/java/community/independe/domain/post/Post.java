2aa74d9f44a888bbe6fe5e22207a90527b20c209
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-08 18:01:33 +0900

    add views and recommendCount in Post Entity refactor Post Constructor

--- src/main/java/community/independe/domain/post/Post.java
+++ src/main/java/community/independe/domain/post/Post.java
@@ -19,6 +19,8 @@ public abstract class Post extends BaseEntity {
     private Long id;
 
     private String title;
+    private Integer views; // 조회수
+    private Integer recommendCount; // 추천수
     @Column(columnDefinition = "text") // 텍스트 타입
     private String content;
 
@@ -27,9 +29,11 @@ public abstract class Post extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member; // 게시글, 회원 N : 1 다대일 단방향 매핑
 
-    public Post(String title, String content, Member member) {
+    public Post(String title, String content, Integer views, Integer recommendCount, Member member) {
         this.title = title;
         this.content = content;
+        this.views = views;
         this.member = member;
+        this.recommendCount = recommendCount;
     }
 }
