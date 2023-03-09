aa76535528fe9731184899561e5987d5209c7ae6
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-09 15:49:03 +0900

    create regionPosts with thymeleaf

--- src/main/java/community/independe/controller/RegionPostController.java
+++ src/main/java/community/independe/controller/RegionPostController.java
@@ -2,6 +2,7 @@ package community.independe.controller;
 
 import community.independe.controller.form.RegionPostForm;
 import community.independe.domain.member.Member;
+import community.independe.domain.post.RegionPost;
 import community.independe.domain.post.enums.RegionPostType;
 import community.independe.domain.post.enums.RegionType;
 import community.independe.service.MemberService;
@@ -9,12 +10,18 @@ import community.independe.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -50,5 +57,21 @@ public class RegionPostController {
 
         postService.createRegionPost(member.getId(), form.getTitle(), form.getContent(), form.getRegionType(), form.getRegionPostType());
         return "redirect:/";
+    }
+
+    @GetMapping("/posts/region/{regionType}/{postType}")
+    public String regionPosts(@PathVariable(value = "regionType") RegionType regionType,
+                              @PathVariable(value = "postType") RegionPostType postType,
+                              @PageableDefault(size = 3) Pageable pageable,
+                              Model model) {
+
+        Page<RegionPost> allRegionPosts = postService.findAllRegionPosts(regionType, postType, pageable);
+        List<RegionPost> content = allRegionPosts.getContent();
+        long totalElements = allRegionPosts.getTotalElements();
+
+        model.addAttribute("regionPost", content);
+        model.addAttribute("totalCount", totalElements);
+
+        return "posts/regionPosts";
     }
 }
