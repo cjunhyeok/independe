0125a36e691dcbb2137bbb74d94fdf7ec0dd5234
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-09 18:33:58 +0900

    add findAllByPostId with simple Test

--- src/main/java/community/independe/repository/CommentRepository.java
+++ src/main/java/community/independe/repository/CommentRepository.java
@@ -2,6 +2,16 @@ package community.independe.repository;
 
 import community.independe.domain.comment.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "select c from Comment c left join fetch c.parent" +
+            " where c.post.id = :postId",
+    countQuery = "select c from Comment c" +
+            " where c.post = :postId")
+    List<Comment> findAllByPostId(@Param("postId") Long PostId);
 }
--- src/test/java/community/independe/repository/CommentRepositoryTest.java
+++ src/test/java/community/independe/repository/CommentRepositoryTest.java
@@ -3,13 +3,20 @@ package community.independe.repository;
 import community.independe.domain.comment.Comment;
 import community.independe.domain.member.Member;
 import community.independe.domain.post.IndependentPost;
+import community.independe.domain.post.RegionPost;
 import community.independe.domain.post.enums.IndependentPostType;
+import community.independe.domain.post.enums.RegionPostType;
+import community.independe.domain.post.enums.RegionType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +29,8 @@ public class CommentRepositoryTest {
     private PostRepository postRepository;
     @Autowired
     private CommentRepository commentRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     public void basicCommentTest() {
@@ -50,5 +59,75 @@ public class CommentRepositoryTest {
 
         Comment findComment = commentRepository.findById(comment.getId()).get();
         Assertions.assertThat(findComment.getContent()).isEqualTo(comment.getContent());
+    }
+
+    @Test
+    public void findByPostIdTest() {
+        Member member = Member.builder()
+                .username("id1")
+                .password("1234")
+                .nickname("nick1")
+                .role("ROLE_USER")
+                .build();
+        memberRepository.save(member);
+
+        RegionPost regionPost = RegionPost.builder()
+                .title("regionTitle")
+                .content("regionContent")
+                .member(member)
+                .regionType(RegionType.ALL)
+                .regionPostType(RegionPostType.RESTAURANT)
+                .build();
+        postRepository.save(regionPost);
+
+        IndependentPost independentPost = IndependentPost.builder()
+                .title("independentTitle")
+                .content("independentContent")
+                .member(member)
+                .independentPostType(IndependentPostType.COOK)
+                .build();
+        postRepository.save(independentPost);
+
+        Comment comment1 = Comment.builder()
+                .content("comment1")
+                .member(member)
+                .post(independentPost)
+                .build();
+        commentRepository.save(comment1);
+
+        Comment comment2 = Comment.builder()
+                .content("comment2")
+                .member(member)
+                .post(independentPost)
+                .build();
+        commentRepository.save(comment2);
+
+        Comment comment3 = Comment.builder()
+                .content("comment3")
+                .member(member)
+                .post(regionPost)
+                .build();
+        commentRepository.save(comment3);
+
+        Comment comment4 = Comment.builder()
+                .content("comment4")
+                .member(member)
+                .post(independentPost)
+                .parent(comment2)
+                .build();
+        commentRepository.save(comment4);
+
+        List<Comment> allByPostId = commentRepository.findAllByPostId(independentPost.getId());
+
+        em.flush();
+        em.clear();
+
+        for (Comment comment : allByPostId) {
+            if (comment.getParent() == null) {
+                continue;
+            }
+            System.out.println(comment.getParent().getContent());
+        }
+        Assertions.assertThat(allByPostId.size()).isEqualTo(3);
     }
 }
