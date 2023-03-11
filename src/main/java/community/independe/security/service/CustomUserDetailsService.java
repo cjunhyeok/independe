d085b617be3dd22c0e40383202967403d671f38a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 15:21:52 +0900

    Create CustomUserDetailsService with MemberContext for JwtSecurity

---
+++ src/main/java/community/independe/security/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package community.independe.security.service;
+
+import community.independe.domain.member.Member;
+import community.independe.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service("userDetailsService")
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        try {
+            Member findMember = memberRepository.findByUsername(username);
+
+            List<GrantedAuthority> roles = new ArrayList<>();
+            roles.add(new SimpleGrantedAuthority(findMember.getRole()));
+
+            return new MemberContext(findMember, roles);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UsernameNotFoundException("Invalid Username");
+        }
+
+    }
+}
---
+++ src/main/java/community/independe/security/service/MemberContext.java
@@ -0,0 +1,21 @@
+package community.independe.security.service;
+
+import community.independe.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class MemberContext extends User {
+
+    private final Member member;
+
+    public MemberContext(Member member, Collection<? extends GrantedAuthority> authorities) {
+        super(member.getUsername(), member.getPassword(), authorities);
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
