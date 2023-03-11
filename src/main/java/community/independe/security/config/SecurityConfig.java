af2e67cdcdc4e5c84cc2dfc10d156f9f9f8f0161
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 15:27:54 +0900

    add userDetailsService in SecurityConfig

--- src/main/java/community/independe/security/config/SecurityConfig.java
+++ src/main/java/community/independe/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,12 +15,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests()
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated();
+
+        http.userDetailsService(userDetailsService);
 
         return http.build();
     }
