1de271ad0847a6ed17294c9f9fb0cc13715bee7f
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 18:03:05 +0900

    disable BasicConfig

--- src/main/java/community/independe/security/config/BasicConfig.java
+++ src/main/java/community/independe/security/config/BasicConfig.java
@@ -13,41 +13,41 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 //@Configuration
 //@EnableWebSecurity
 //@RequiredArgsConstructor
-public class BasicConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf().disable(); // csrf 비활성화
-
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        http.formLogin();
-
-//        http.cors().configurationSource(corsConfigurationSource()); // cors 설정
-
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("localhost:8080");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-
-    }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-}
+//public class BasicConfig {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable(); // csrf 비활성화
+//
+//        http.authorizeHttpRequests().anyRequest().permitAll();
+//        http.formLogin();
+//
+////        http.cors().configurationSource(corsConfigurationSource()); // cors 설정
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.addAllowedOrigin("*");
+//        configuration.addAllowedMethod("*");
+//        configuration.addAllowedHeader("localhost:8080");
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return source;
+//
+//    }
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//}
