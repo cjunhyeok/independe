c234d7435ae1c2021c9b259f5056fb34c8950ec8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 16:24:09 +0900

    create SignatureConfig in mac algorithm

---
+++ src/main/java/community/independe/security/config/SignatureConfig.java
@@ -0,0 +1,28 @@
+package community.independe.security.config;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import community.independe.security.signature.MacSecuritySigner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SignatureConfig {
+
+    @Bean
+    public MacSecuritySigner macSecuritySigner() {
+        return new MacSecuritySigner();
+    }
+
+    @Bean
+    public OctetSequenceKey octetSequenceKey() throws JOSEException {
+        OctetSequenceKey macKey = new OctetSequenceKeyGenerator(256)
+                .keyID("macKey")
+                .algorithm(JWSAlgorithm.HS256)
+                .generate();
+
+        return macKey;
+    }
+}
