6d371e0f13006116333de7dc29e967d6f24077e2
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 14:55:15 +0900

    create Basic Security config for Jwt and Create CorsConfig Class

---
+++ src/main/java/community/independe/security/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package community.independe.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
