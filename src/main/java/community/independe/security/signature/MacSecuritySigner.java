e55f9760f9bab1435b51eee52069f0df38f00276
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 16:23:11 +0900

    Create SecuritySigner with MacSecuritySigner signer provide jwt token in mac algorithm

---
+++ src/main/java/community/independe/security/signature/MacSecuritySigner.java
@@ -0,0 +1,18 @@
+package community.independe.security.signature;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import org.springframework.security.core.userdetails.UserDetails;
+
+// 대칭키
+public class MacSecuritySigner extends SecuritySigner{
+
+    @Override
+    public String getToken(UserDetails user, JWK jwk) throws JOSEException {
+        // 토큰 발행
+        MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey()); // 시크릿 키를 이용해 Signer 생성
+        return super.getJwtTokenInternal(jwsSigner, user, jwk); // Token 발행은 공통이므로 부모에게 전달
+    }
+}
---
+++ src/main/java/community/independe/security/signature/SecuritySigner.java
@@ -0,0 +1,36 @@
+package community.independe.security.signature;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public abstract class SecuritySigner {
+
+    protected String getJwtTokenInternal(MACSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
+
+        // jwk에서 알고리즘 정보와 keyId를 가져와 Header를 생성
+        JWSHeader header = new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
+        // Claim정보 생성
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject("user")
+                .issuer("http://localhost:8080")
+                .claim("username", user.getUsername()) // 사용자 Id
+                .claim("authority", user.getAuthorities()) // 사용자 권한
+                .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 10)) // 10분
+                .build();
+        SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
+        signedJWT.sign(jwsSigner); // mac signer를 이용해 서명
+        String jwtToken = signedJWT.serialize();
+
+        return jwtToken;
+    }
+
+    public abstract String getToken(UserDetails user, JWK jwk) throws JOSEException;
+}
