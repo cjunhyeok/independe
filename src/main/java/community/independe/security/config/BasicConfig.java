1bf916b69a94414c0e15bb59e9cf251ac3a24ca3
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-11 14:54:34 +0900

    disable BasicConfig for Jwt Security

--- src/main/java/community/independe/security/config/BasicConfig.java
+++ src/main/java/community/independe/security/config/BasicConfig.java
@@ -1,10 +1,7 @@
 package community.independe.security.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,9 +9,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
+
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
 public class BasicConfig {
 
     @Bean
