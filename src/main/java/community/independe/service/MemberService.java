dfb5889636409b13f4f07daab13dc5a99e512e96
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-14 00:50:58 +0900

    refactor memberService join parameter

--- src/main/java/community/independe/service/MemberService.java
+++ src/main/java/community/independe/service/MemberService.java
@@ -4,7 +4,7 @@ import community.independe.domain.member.Member;
 
 public interface MemberService {
 
-    Long join(Member member);
+    Long join(String username, String password, String nickname, String email, String number, String city, String street, String zipcode);
 
     Member findById(Long id);
 }
--- src/main/java/community/independe/service/MemberServiceImpl.java
+++ src/main/java/community/independe/service/MemberServiceImpl.java
@@ -1,9 +1,11 @@
 package community.independe.service;
 
+import community.independe.domain.member.Address;
 import community.independe.domain.member.Member;
 import community.independe.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +16,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
-    @Transactional
-    public Long join(Member member) {
+    public Long join(String username, String password, String nickname, String email, String number, String city, String street, String zipcode) {
+        Member member = Member.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .nickname(nickname)
+                .role("ROLE_USER")
+                .email(email)
+                .number(number)
+                .address(new Address(city, street, zipcode))
+                .build();
         Member savedMember = memberRepository.save(member);
         return savedMember.getId();
     }
