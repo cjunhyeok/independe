735f6c06b0453f1269c8e4e1f33f9a67c596e2eb
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-15 13:12:19 +0900

    Refactor createParentComment HTTP method GET to POST

--- src/main/java/community/independe/api/CommentApiController.java
+++ src/main/java/community/independe/api/CommentApiController.java
@@ -6,7 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class CommentApiController {
 
     private final CommentService commentService;
 
-    @GetMapping("/api/comments/parent/new")
+    @PostMapping("/api/comments/parent/new")
     public ResponseEntity<Long> createParentComment(@RequestBody @Valid CreateParentCommentRequest request) {
         Long parentComment = commentService.createParentComment(
                 request.getMemberId(),
