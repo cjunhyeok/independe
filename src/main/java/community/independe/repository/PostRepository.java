713d963056df8b6d9ba996567e601c6f1edc2320
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-16 15:27:41 +0900

    Refactor Post Entity refactor PostRepository for @Param

--- src/main/java/community/independe/repository/PostRepository.java
+++ src/main/java/community/independe/repository/PostRepository.java
@@ -42,18 +42,18 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllRegionPostsWithMember(Pageable pageable);
 
     @Query(value = "select p from Post p join fetch p.member" +
-            " where p.independentPostType =: independentPostType",
+            " where p.independentPostType = :independentPostType",
     countQuery = "select count(p) from Post p" +
             " where p.independentPostType =: independentPostType")
     Page<Post> findAllIndependentPostsByTypeWithMember(@Param("independentPostType") IndependentPostType independentPostType,
                                                        Pageable pageable);
 
     @Query(value = "select p from Post p join fetch p.member" +
-            " where p.regionType =: regionType" +
-            " and p.regionPostType =: regionPostType",
+            " where p.regionType = :regionType" +
+            " and p.regionPostType = :regionPostType",
     countQuery = "select count(p) from Post p" +
-            " where p.regionPostType =: regionType" +
-            " and p.regionPostType =: regionPostType")
+            " where p.regionPostType = :regionType" +
+            " and p.regionPostType = :regionPostType")
     Page<Post> findAllRegionPostsByTypesWithMember(@Param("regionType")RegionType regionType,
                                                    @Param("regionPostType")RegionPostType regionPostType,
                                                    Pageable pageable);
