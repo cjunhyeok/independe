8b966f485b5871556c8a16691a6070debe9c8b21
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-16 14:53:53 +0900

    Refactor Post Entity add findAllIndependentPosts and findAllRegionPosts for tests

--- src/main/java/community/independe/repository/PostRepository.java
+++ src/main/java/community/independe/repository/PostRepository.java
@@ -10,7 +10,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p" +
+            " where p.independentPostType IS NOT NULL")
+    List<Post> findAllIndependentPosts();
+
+    @Query("select p from Post p" +
+            " where p.regionType IS NOT NULL" +
+            " and p.regionPostType IS NOT NULL")
+    List<Post> findAllRegionPosts();
 
     @Query("select p from Post p join fetch p.member")
     Page<Post> findAllPostsWithMember(Pageable pageable);
