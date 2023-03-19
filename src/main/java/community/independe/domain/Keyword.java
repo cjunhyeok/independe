58eeb3ef9b787800a72add72085fd58faea98c61
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-19 18:40:08 +0900

    Refactor Keyword Entity delete KeywordCount and extends BaseEntity

--- src/main/java/community/independe/domain/Keyword.java
+++ src/main/java/community/independe/domain/Keyword.java
@@ -4,24 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Keyword {
+public class Keyword extends BaseEntity{
 
     @Id @GeneratedValue
     private Long id;
 
     private String keywordName;
-    private Long keywordCount;
 
-    @Builder
     public Keyword(String keywordName) {
         this.keywordName = keywordName;
-        this.keywordCount = 0L;
     }
 }
