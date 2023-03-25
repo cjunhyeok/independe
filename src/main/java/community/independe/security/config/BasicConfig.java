761783f43ff4d3a8e5b707cbd851079a9d7bb003
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-03-25 15:46:40 +0900

    set basic config add Origin for android

--- src/main/java/community/independe/security/config/BasicConfig.java
+++ src/main/java/community/independe/security/config/BasicConfig.java
@@ -1,7 +1,10 @@
 package community.independe.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,44 +13,40 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class BasicConfig {
-//
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http.csrf().disable(); // csrf 비활성화
-//
-//        http.authorizeHttpRequests().anyRequest().permitAll();
-//        http.formLogin();
-//
-////        http.cors().configurationSource(corsConfigurationSource()); // cors 설정
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-//        configuration.addAllowedOrigin("*");
-//        configuration.addAllowedMethod("*");
-//        configuration.addAllowedHeader("localhost:8080");
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//
-//        return source;
-//
-//    }
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//}
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class BasicConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable(); // csrf 비활성화
+
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.formLogin();
+
+        http.cors().configurationSource(corsConfigurationSource()); // cors 설정
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+//        config.addAllowedOrigin("http://192.168.0.13:8080"); // vue
+        config.addAllowedOrigin("http://192.168.0.5:8080"); // android
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/api/**", config);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
