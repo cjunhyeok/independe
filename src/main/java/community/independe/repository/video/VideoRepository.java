25a3be59361f27289e4a1ec1f288f9fcae60d8ad
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-01 19:27:19 +0900

    add findAllByIndependentPostType in Repository

--- src/main/java/community/independe/repository/video/VideoRepository.java
+++ src/main/java/community/independe/repository/video/VideoRepository.java
@@ -1,9 +1,17 @@
 package community.independe.repository.video;
 
 
+import community.independe.domain.post.enums.IndependentPostType;
 import community.independe.domain.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface VideoRepository extends JpaRepository<Video, Long>, VideoRepositoryCustom {
 
+    @Query(value = "select v from Video v" +
+            " where v.independentPostType = :independentPostType")
+    List<Video> findAllByIndependentPostType(@Param("independentPostType")IndependentPostType independentPostType);
 }
