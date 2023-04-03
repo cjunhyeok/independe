1c4e55fb1022c2306180545378f1c3467ba50b65
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-03 19:48:46 +0900

    Refactor findAllMethods firstResult and maxResult set parameter

--- src/main/java/community/independe/repository/query/PostApiRepository.java
+++ src/main/java/community/independe/repository/query/PostApiRepository.java
@@ -15,52 +15,52 @@ public class PostApiRepository {
 
     private final EntityManager em;
 
-    public List<Post> findAllPopularPosts(LocalDateTime yesterday, LocalDateTime today) {
+    public List<Post> findAllPopularPosts(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
         return em.createQuery("select p from Post p" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                         " order by p.views", Post.class)
-                .setFirstResult(0)
-                .setMaxResults(10)
+                .setFirstResult(first)
+                .setMaxResults(max)
                 .setParameter("yesterday", yesterday)
                 .setParameter("today", today)
                 .getResultList();
     }
 
-    public List<Post> findAllIndependentPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today) {
+    public List<Post> findAllIndependentPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
         return em.createQuery("select p from Post p" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                 " and p.independentPostType IS NOT NULL" +
                 " order by p.recommendCount", Post.class)
                 .setParameter("yesterday", yesterday)
                 .setParameter("today", today)
-                .setFirstResult(0)
-                .setMaxResults(10)
+                .setFirstResult(first)
+                .setMaxResults(max)
                 .getResultList();
     }
 
-    public List<Post> findAllRegionAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today) {
+    public List<Post> findAllRegionAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
         return em.createQuery("select p from Post p" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                 " and p.regionType IS NOT NULL" +
                 " and p.regionPostType IS NOT NULL" +
                 " and p.regionType = :regionType" +
                 " order by p.recommendCount", Post.class)
-                .setFirstResult(0)
-                .setMaxResults(5)
+                .setFirstResult(first)
+                .setMaxResults(max)
                 .setParameter("yesterday", yesterday)
                 .setParameter("today", today)
                 .setParameter("regionType", RegionType.ALL)
                 .getResultList();
     }
 
-    public List<Post> findRegionNotAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today) {
+    public List<Post> findRegionNotAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
         return em.createQuery("select p from Post p" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                 " and p.independentPostType IS NULL" +
                 " and p.regionType <> :regionType" +
                 " order by p.recommendCount", Post.class)
-                .setFirstResult(0)
-                .setMaxResults(5)
+                .setFirstResult(first)
+                .setMaxResults(max)
                 .setParameter("yesterday", yesterday)
                 .setParameter("today", today)
                 .setParameter("regionType", RegionType.ALL)
