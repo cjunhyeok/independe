476394768b366c27575179c8746272b343db9854
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-05 20:14:54 +0900

    Refactor PostsResponse add postId

--- src/main/java/community/independe/api/dtos/post/PostsResponse.java
+++ src/main/java/community/independe/api/dtos/post/PostsResponse.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PostsResponse {
 
+    private Long postId;
     private String nickName;
     private String title;
     private LocalDateTime lastModifiedDate;
