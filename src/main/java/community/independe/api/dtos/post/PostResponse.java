769ffa3462cf558b11b845c79b6c83cc5511a3dd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-08 15:42:18 +0900

    Refactor PostResponse for Files

--- src/main/java/community/independe/api/dtos/post/PostResponse.java
+++ src/main/java/community/independe/api/dtos/post/PostResponse.java
@@ -1,16 +1,24 @@
 package community.independe.api.dtos.post;
 
 import community.independe.domain.comment.Comment;
+import community.independe.domain.file.Files;
 import community.independe.domain.post.Post;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.UrlResource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
+@Slf4j
 public class PostResponse {
 
     private String title;
@@ -20,8 +28,9 @@ public class PostResponse {
     private Integer views;
     private Integer recommendCount;
     private List<PostCommentResponse> comments;
+    private List<byte[]> files;
 
-    public PostResponse(Post post, List<Comment> comments) {
+    public PostResponse(Post post, List<Comment> comments, List<Files> files) {
         this.title = post.getTitle();
         this.content = post.getContent();
         this.nickname = post.getMember().getNickname();
@@ -29,8 +38,29 @@ public class PostResponse {
         this.views = post.getViews();
         this.recommendCount = post.getRecommendCount();
         this.comments = comments.stream()
-                .map(c -> new PostCommentResponse(c))
+                .map(PostCommentResponse::new)
                 .collect(Collectors.toList());
+        this.files = files.stream()
+                .map(f -> {
+                    try {
+                        return getImageBytes(f, post.getId());
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    public byte[] getImageBytes(Files files, Long postId) throws IOException {
+        UrlResource resource = new UrlResource(files.getFilePath() + "/" + postId + "/" + files.getStoreFilename());
+
+        InputStream inputStream = resource.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        inputStream.close();
+
+        return bytes;
     }
 
 }
