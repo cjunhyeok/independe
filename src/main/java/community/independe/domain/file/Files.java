b0bf2c0218ae6fe775f8bf82f71c28e74d1db25b
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-08 14:39:00 +0900

    Refactor File Entity name to Files

--- src/main/java/community/independe/domain/file/Files.java
+++ src/main/java/community/independe/domain/file/Files.java
@@ -12,7 +12,7 @@ import static jakarta.persistence.FetchType.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class File {
+public class Files {
 
     @Id
     @GeneratedValue
@@ -21,15 +21,17 @@ public class File {
 
     private String uploadFilename; // 사용자가 업로드한 파일명
     private String storeFilename; // 서버에 저장한 파일명
+    private String filePath; // 파일 저장 경로
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "post_id")
     private Post post; // 파일, 게시글 -> N : 1
 
     @Builder
-    public File(String uploadFilename, String storeFilename, Post post) {
+    public Files(String uploadFilename, String storeFilename, String filePath, Post post) {
         this.uploadFilename = uploadFilename;
         this.storeFilename = storeFilename;
+        this.filePath = filePath;
         this.post = post;
     }
 }
