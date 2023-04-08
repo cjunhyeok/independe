f4dbd994f42e74f259289f76b8e960166272ce89
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-08 14:39:27 +0900

    Create FilesRepository with JpaRepository

---
+++ src/main/java/community/independe/repository/file/FilesRepository.java
@@ -0,0 +1,8 @@
+package community.independe.repository.file;
+
+import community.independe.domain.file.Files;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilesRepository extends JpaRepository<Files, Long> {
+
+}
