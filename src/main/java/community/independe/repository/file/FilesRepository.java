ddfa78d6514ba9bb79887aeadedbecb1830d14c1
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-08 15:38:28 +0900

    add findAllFilesByPostId in FilesRepository

--- src/main/java/community/independe/repository/file/FilesRepository.java
+++ src/main/java/community/independe/repository/file/FilesRepository.java
@@ -2,7 +2,16 @@ package community.independe.repository.file;
 
 import community.independe.domain.file.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FilesRepository extends JpaRepository<Files, Long> {
 
+    @Query(value = "select f from Files f left join fetch f.post" +
+            " where f.post.id = :postId",
+    countQuery = "select f from Files f" +
+            " where f.post.id =: postId")
+    List<Files> findAllFilesByPostId(@Param("postId") Long postId);
 }
