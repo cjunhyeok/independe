922fe146cca8f63b86a2ba2ce33820aad0441b31
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-13 14:58:45 +0900

    Add IsUpDto for increaseOrDecreaseRecommendCount

---
+++ src/main/java/community/independe/api/dtos/IsUpDto.java
@@ -0,0 +1,13 @@
+package community.independe.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IsUpDto {
+
+    private Boolean isUp;
+}
