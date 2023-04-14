ac94da66ca89de115187d95c1d09d8a31587552f
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-14 19:03:16 +0900

    Create RecommendPost with BaseManyToManyEntity (N:M)

---
+++ src/main/java/community/independe/domain/manytomany/RecommendPost.java
@@ -0,0 +1,39 @@
+package community.independe.domain.manytomany;
+
+import community.independe.domain.member.Member;
+import community.independe.domain.post.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecommendPost extends BaseManyToManyEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "recommend_post_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private Boolean isFavorite;
+
+    @Builder
+    public RecommendPost(Member member, Post post) {
+        this.member = member;
+        this.post = post;
+        this.isFavorite = true;
+    }
+}
