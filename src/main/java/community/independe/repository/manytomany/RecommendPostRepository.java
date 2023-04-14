256f7f8e7331f0976df783e923c695e2c6228d50
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-14 19:30:21 +0900

    Add countAllByPostIdAndIsRecommend in RecommendPostRepository

--- src/main/java/community/independe/repository/manytomany/RecommendPostRepository.java
+++ src/main/java/community/independe/repository/manytomany/RecommendPostRepository.java
@@ -2,6 +2,13 @@ package community.independe.repository.manytomany;
 
 import community.independe.domain.manytomany.RecommendPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecommendPostRepository extends JpaRepository<RecommendPost, Long> {
+
+    @Query("select count(r) from RecommendPost r" +
+            " where r.post.id = :postId" +
+            " and r.isRecommend = true")
+    Long countAllByPostIdAndIsRecommend(@Param("postId") Long postId);
 }
