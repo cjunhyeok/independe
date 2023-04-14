82d82c453ffef00e4c30938804adbc32bb4b9254
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-14 20:03:50 +0900

    Create RecommendPostApiController with addRecommendPost 2L is test for API

---
+++ src/main/java/community/independe/api/manytomany/RecommendPostApiController.java
@@ -0,0 +1,43 @@
+package community.independe.api.manytomany;
+
+import community.independe.domain.manytomany.RecommendPost;
+import community.independe.domain.member.Member;
+import community.independe.service.PostService;
+import community.independe.service.manytomany.RecommendPostService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class RecommendPostApiController {
+
+    private final RecommendPostService recommendPostService;
+    private final PostService postService;
+
+    @Operation(summary = "게시글 추천")
+    @PostMapping("/api/recommendPost/{postId}")
+    public ResponseEntity<Long> addRecommendPost(@PathVariable(name = "postId") Long postId,
+                                                 @AuthenticationPrincipal Member member) {
+
+//        RecommendPost findRecommendPostByPost = recommendPostService.findByPostIdAndMemberId(postId, member.getId());
+        RecommendPost findRecommendPostByPost = recommendPostService.findByPostIdAndMemberId(postId,2L);
+
+        if (findRecommendPostByPost == null) {
+//            recommendPostService.save(postId, member.getId());
+            recommendPostService.save(postId, 2L);
+        } else if(findRecommendPostByPost.getIsRecommend() == false) {
+            recommendPostService.updateIsRecommend(findRecommendPostByPost, true);
+        } else if(findRecommendPostByPost.getIsRecommend() == true) {
+            recommendPostService.updateIsRecommend(findRecommendPostByPost, false);
+        }
+
+        return null;
+    }
+}
