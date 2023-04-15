b19f8b0ba6c8264a76e409f6255a4227c4e6c9dd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-15 15:01:59 +0900

    Add findByPostIdAndMemberId in FavoritePostRepository

--- src/main/java/community/independe/repository/manytomany/FavoritePostRepository.java
+++ src/main/java/community/independe/repository/manytomany/FavoritePostRepository.java
@@ -2,6 +2,18 @@ package community.independe.repository.manytomany;
 
 import community.independe.domain.manytomany.FavoritePost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FavoritePostRepository extends JpaRepository<FavoritePost, Long> {
+
+    @Query(value = "select f from FavoritePost f join fetch f.post" +
+            " join fetch f.member" +
+            " where f.post.id = :postId" +
+            " and f.member.id = :memberId",
+    countQuery = "select f from FavoritePost f" +
+            " where f.post.id = :postId" +
+            " and f.member.id = :memberId")
+    FavoritePost findByPostIdAndMemberId(@Param("postId") Long postId,
+                                         @Param("memberId") Long memberId);
 }
