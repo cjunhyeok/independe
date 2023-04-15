e6a799a514aab5cd65c4dc5fc59324a8ab84119e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-15 16:07:10 +0900

    Create ReportPostApiController with addReportPost

---
+++ src/main/java/community/independe/api/manytomany/ReportPostApiController.java
@@ -0,0 +1,38 @@
+package community.independe.api.manytomany;
+
+import community.independe.domain.manytomany.ReportPost;
+import community.independe.domain.member.Member;
+import community.independe.service.manytomany.ReportPostService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ReportPostApiController {
+
+    private final ReportPostService reportPostService;
+
+    @Operation(summary = "게시글 신고")
+    @PostMapping("/api/reportPost/{postId}")
+    public ResponseEntity addReportPost(@PathVariable(name = "postId") Long postId,
+                                        @AuthenticationPrincipal Member member) {
+        ReportPost findReportPost = reportPostService.findByPostIdAndMemberId(postId, member.getId());
+
+        if (findReportPost == null) {
+            reportPostService.save(postId, member.getId());
+        } else if (findReportPost.getIsReport() == false) {
+            reportPostService.updateIsReport(findReportPost, true);
+        } else if (findReportPost.getIsReport() == true) {
+            reportPostService.updateIsReport(findReportPost, false);
+        }
+
+        return ResponseEntity.ok("OK");
+    }
+}
