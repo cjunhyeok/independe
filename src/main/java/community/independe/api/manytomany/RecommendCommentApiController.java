95ac2acbd3883bde0a8167bd512a79184289dd94
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-16 14:34:56 +0900

    Create RecommendCommentApiController with addRecommendComment

---
+++ src/main/java/community/independe/api/manytomany/RecommendCommentApiController.java
@@ -0,0 +1,44 @@
+package community.independe.api.manytomany;
+
+import community.independe.domain.manytomany.RecommendComment;
+import community.independe.domain.member.Member;
+import community.independe.service.manytomany.RecommendCommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class RecommendCommentApiController {
+
+    private final RecommendCommentService recommendCommentService;
+
+    @Operation(summary = "댓글 추천")
+    @PostMapping("/api/recommendComment/{commentId}")
+    public ResponseEntity addRecommendComment(@PathVariable(name = "commentId") Long commentId,
+                                              @AuthenticationPrincipal Member member) {
+
+//        RecommendComment findRecommendComment = recommendCommentService.findByCommentIdAndMemberId(
+//                commentId, member.getId());
+
+        RecommendComment findRecommendComment = recommendCommentService.findByCommentIdAndMemberId(
+                commentId, 1L);
+
+        if (findRecommendComment == null) {
+//            Long savedRecommendComment = recommendCommentService.save(commentId, member.getId());
+            Long savedRecommendComment = recommendCommentService.save(commentId, 1L);
+        } else if (findRecommendComment.getIsRecommend() == false) {
+            recommendCommentService.updateIsRecommend(findRecommendComment, true);
+        } else if (findRecommendComment.getIsRecommend() == true) {
+            recommendCommentService.updateIsRecommend(findRecommendComment, false);
+        }
+
+        return ResponseEntity.ok("OK");
+    }
+}
