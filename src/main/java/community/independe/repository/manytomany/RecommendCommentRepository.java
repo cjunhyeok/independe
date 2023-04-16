f80473e4c521ba0d0258f3bd65f6e8480ef55a34
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-16 14:22:27 +0900

    Add findByCommentIdAndMemberId in RecommendCommentRepository

--- src/main/java/community/independe/repository/manytomany/RecommendCommentRepository.java
+++ src/main/java/community/independe/repository/manytomany/RecommendCommentRepository.java
@@ -2,6 +2,18 @@ package community.independe.repository.manytomany;
 
 import community.independe.domain.manytomany.RecommendComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecommendCommentRepository extends JpaRepository<RecommendComment, Long> {
+
+    @Query(value = "select r from RecommendComment r join fetch r.comment" +
+            " join fetch r.member" +
+            " where r.comment.id = :commentId" +
+            " and r.member.id = :memberId",
+    countQuery = "select r from RecommendComment r" +
+            " where r.comment.id = :commentId" +
+            " and r.member.id = :memberId")
+    RecommendComment findByCommentIdAndMemberId(@Param("commentId") Long commentId,
+                                                @Param("memberId") Long memberId);
 }
