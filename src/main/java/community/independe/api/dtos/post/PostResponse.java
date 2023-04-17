3e89499a97c363ff99e517757605cb0c9dbad0dc
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-17 13:56:19 +0900

    Refactor PostResponse Dto

--- src/main/java/community/independe/api/dtos/post/PostResponse.java
+++ src/main/java/community/independe/api/dtos/post/PostResponse.java
@@ -1,6 +1,5 @@
 package community.independe.api.dtos.post;
 
-import community.independe.domain.file.Files;
 import community.independe.domain.post.Post;
 import community.independe.domain.post.enums.IndependentPostType;
 import community.independe.domain.post.enums.RegionPostType;
@@ -8,15 +7,8 @@ import community.independe.domain.post.enums.RegionType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.UrlResource;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -39,10 +31,10 @@ public class PostResponse {
     private Boolean isRecommend;
     private Boolean isFavorite;
     private Boolean isReport;
+    private BestCommentDto bestComment;
     private List<PostCommentResponse> comments;
-    private List<byte[]> files;
 
-    public PostResponse(Post post, List<PostCommentResponse> comments, List<Files> files, Long commentCount, Long recommendCount, Boolean isRecommend, Boolean isFavorite, Boolean isReport) {
+    public PostResponse(Post post, BestCommentDto bestComment ,List<PostCommentResponse> comments, Long commentCount, Long recommendCount, Boolean isRecommend, Boolean isFavorite, Boolean isReport) {
         this.title = post.getTitle();
         this.content = post.getContent();
         this.nickname = post.getMember().getNickname();
@@ -59,28 +51,8 @@ public class PostResponse {
         this.isRecommend = isRecommend;
         this.isFavorite = isFavorite;
         this.isReport = isReport;
+        this.bestComment = bestComment;
         this.comments = comments;
-        this.files = files.stream()
-                .map(f -> {
-                    try {
-                        return getImageBytes(f, post.getId());
-                    } catch (MalformedURLException e) {
-                        throw new RuntimeException(e);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .collect(Collectors.toList());
-    }
-
-    public byte[] getImageBytes(Files files, Long postId) throws IOException {
-        UrlResource resource = new UrlResource("file:" + files.getFilePath());
-
-        InputStream inputStream = resource.getInputStream();
-        byte[] bytes = IOUtils.toByteArray(inputStream);
-        inputStream.close();
-
-        return bytes;
     }
 
 }
