1fc97af748b3fc88f46693e381499104ae3ce2bb
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-09 20:36:10 +0900

    Refactor MemberContext implements OAuth2User for OAuth2 Login

--- src/main/java/community/independe/security/service/MemberContext.java
+++ src/main/java/community/independe/security/service/MemberContext.java
@@ -3,12 +3,21 @@ package community.independe.security.service;
 import community.independe.domain.member.Member;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
-public class MemberContext extends User {
+public class MemberContext extends User implements OAuth2User {
 
     private final Member member;
+    private Map<String, Object> attributes;
+
+    public MemberContext(Member member, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes) {
+        super(member.getUsername(), member.getPassword(), authorities);
+        this.member = member;
+        this.attributes = attributes;
+    }
 
     public MemberContext(Member member, Collection<? extends GrantedAuthority> authorities) {
         super(member.getUsername(), member.getPassword(), authorities);
@@ -17,5 +26,16 @@ public class MemberContext extends User {
 
     public Member getMember() {
         return member;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+//        return attributes.get("sub");
+        return null;
     }
 }
