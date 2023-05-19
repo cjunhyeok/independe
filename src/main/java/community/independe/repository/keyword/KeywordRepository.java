4419346ed6a8585afb1f61a791500b273a7be980
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-19 18:47:47 +0900

    Refactor KeywordRepository delete not use methods

--- src/main/java/community/independe/repository/keyword/KeywordRepository.java
+++ src/main/java/community/independe/repository/keyword/KeywordRepository.java
@@ -3,12 +3,5 @@ package community.independe.repository.keyword;
 import community.independe.domain.keyword.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface KeywordRepository extends JpaRepository<Keyword, Long>, KeywordRepositoryCustom {
-
-    Long countAllByKeyword(String keyword);
-
-    List<Keyword> findAllByKeyword(String keyword);
-
 }
