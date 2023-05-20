75a796adb0ca9bf72cbbbd53780ced3feab0ae90
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-20 18:25:11 +0900

    Refactor simple MemberServiceTest

--- src/test/java/community/independe/service/MemberServiceTest.java
+++ src/test/java/community/independe/service/MemberServiceTest.java
@@ -1,35 +1,60 @@
 package community.independe.service;
 
 import community.independe.domain.member.Member;
-import org.assertj.core.api.Assertions;
+import community.independe.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest
-@Rollback(value = false)
+import java.lang.reflect.Field;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
 
-    @Autowired
-    private MemberService memberService;
+    @InjectMocks
+    private MemberServiceImpl memberService;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @Test
     public void joinTest() {
 
         // given
-        Member member = Member.builder()
-                .username("id1")
-                .password("1234")
-                .nickname("nickname")
-                .role("ROLE_USER")
-                .build();
+        String username = "id";
+        String password = "1234";
+        String nickname = "nick";
+
+        // stub
+        when(memberRepository.findByUsername(username)).thenReturn(null);
+        when(passwordEncoder.encode(password)).thenReturn("hashedPassword");
+        when(memberRepository.save(any(Member.class))).thenAnswer(invocation -> {
+            Member member = invocation.getArgument(0);
+            setPrivateField(member, "id", 1L);  // Reflection으로 ID 값을 설정
+            return member;
+        });
 
         // when
-        Long joinMemberId = memberService.join("id1", "1234", "nickname", null, null, null, null, null);
-        Member findMember = memberService.findById(joinMemberId);
+        Long joinMemberId = memberService.join(username, password, nickname, null, null, null, null, null);
 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        System.out.println(joinMemberId);
 
+        verify(memberRepository).findByUsername(username);
+        verify(passwordEncoder).encode(password);
+        verify(memberRepository).save(any(Member.class));
+    }
+
+    private void setPrivateField(Object object, String fieldName, Object value) throws Exception {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(object, value);
     }
 }
