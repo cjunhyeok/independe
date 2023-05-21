6282d9305bba630ef2a8712034f695f2c3672cc9
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-21 13:26:56 +0900

    Refactor Simple CommentServiceTest

--- src/test/java/community/independe/service/CommentServiceTest.java
+++ src/test/java/community/independe/service/CommentServiceTest.java
@@ -1,57 +1,179 @@
 package community.independe.service;
 
 import community.independe.domain.comment.Comment;
-import community.independe.domain.post.enums.IndependentPostType;
-import org.assertj.core.api.Assertions;
+import community.independe.domain.member.Member;
+import community.independe.domain.post.Post;
+import community.independe.repository.CommentRepository;
+import community.independe.repository.MemberRepository;
+import community.independe.repository.post.PostRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
-@Rollback(value = false)
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
 
-    @Autowired
-    private MemberService memberService;
-    @Autowired
-    private PostService postService;
-    @Autowired
-    private CommentService commentService;
+    @InjectMocks
+    private CommentServiceImpl commentService;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Test
+    public void findByIdTest() {
+        // given
+        Long id = 1L;
+        Comment mockComment = Comment.builder().build();
+
+        // stub
+        when(commentRepository.findById(id)).thenReturn(Optional.of(mockComment));
+
+        // when
+        Comment findComment = commentService.findById(id);
+
+        // then
+        verify(commentRepository, times(1)).findById(id);
+        assertThat(findComment).isEqualTo(mockComment);
+    }
+
+    @Test
+    public void findByIdFailTest() {
+        // given
+        Long id = 1L;
+
+        // stub
+        when(commentRepository.findById(id)).thenReturn(Optional.empty());
+
+        // when
+        IllegalArgumentException exception = org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commentService.findById(id));
+
+        // then
+        verify(commentRepository, times(1)).findById(id);
+        assertThat(exception).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     public void createParentCommentTest() {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        String content = "parentContent";
 
-        Long joinMemberId = memberService.join("id1", "1234", "nickname", null, null, null, null, null);
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(Member.builder().build()));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(Post.builder().build()));
+        when(commentRepository.save(any(Comment.class))).thenReturn(Comment.builder().build());
 
-        String title = "title";
-        String content = "content";
-        IndependentPostType independentPostType = IndependentPostType.COOK;
+        // when
+        commentService.createParentComment(memberId, postId, content);
 
-        Long independentPostId = postService.createIndependentPost(joinMemberId, title, content, independentPostType);
+        // then
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(postRepository, times(1)).findById(postId);
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
 
-        Long parentId = commentService.createParentComment(joinMemberId, independentPostId, "parent");
+    @Test
+    public void createParentCommentMemberFailTest() {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        String content = "parentContent";
 
-        Comment findComment = commentService.findById(parentId);
-        Assertions.assertThat(findComment.getId()).isEqualTo(parentId);
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commentService.createParentComment(memberId, postId, content));
+
+        //
+        verify(memberRepository).findById(memberId);
+        verifyNoInteractions(postRepository);
+        verifyNoInteractions(commentRepository);
+        assertThat(exception).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void createParentCommentPostFailTest() {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        String content = "parentContent";
+
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(Member.builder().build()));
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commentService.createParentComment(memberId, postId, content));
+
+        // then
+        verify(memberRepository).findById(memberId);
+        verify(postRepository).findById(postId);
+        verifyNoInteractions(commentRepository);
+        assertThat(exception).isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void createChildCommentTest() {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        Long commentId = 1L;
+        String content = "childComment";
 
-        Long joinMemberId = memberService.join("id1", "1234", "nickname", null, null, null, null, null);
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(Member.builder().build()));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(Post.builder().build()));
+        when(commentRepository.findById(commentId)).thenReturn(Optional.of(Comment.builder().build()));
+        when(commentRepository.save(any(Comment.class))).thenReturn(Comment.builder().build());
 
-        String title = "title";
-        String content = "content";
-        IndependentPostType independentPostType = IndependentPostType.COOK;
+        // when
+        commentService.createChildComment(memberId, postId, commentId, content);
 
-        Long independentPostId = postService.createIndependentPost(joinMemberId, title, content, independentPostType);
+        // then
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(postRepository, times(1)).findById(postId);
+        verify(commentRepository, times(1)).findById(commentId);
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
 
-        Long parentId = commentService.createParentComment(joinMemberId, independentPostId, "parent");
+    @Test
+    public void createParentCommentFailTest() {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        Long commentId = 1L;
+        String content = "childContent";
 
-        Long childId = commentService.createChildComment(joinMemberId, independentPostId, parentId, "child");
-        Comment childComment = commentService.findById(childId);
-        Comment parentComment = commentService.findById(parentId);
-        Assertions.assertThat(childComment.getParent().getId()).isEqualTo(parentComment.getId());
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(Member.builder().build()));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(Post.builder().build()));
+        when(commentRepository.findById(commentId)).thenReturn(Optional.empty());
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> commentService.createChildComment(memberId, postId, commentId, content));
+
+        // then
+        verify(memberRepository).findById(memberId);
+        verify(postRepository).findById(postId);
+        verify(commentRepository).findById(postId);
+        verifyNoMoreInteractions(commentRepository);
+        assertThat(exception).isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
