38dec7c1a7a007322ca1a96d0939bed4c56ba0ea
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-25 14:25:20 +0900

    Refactor SecuritySigner add nickname claim for vue

--- src/main/java/community/independe/security/signature/SecuritySigner.java
+++ src/main/java/community/independe/security/signature/SecuritySigner.java
@@ -7,6 +7,8 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import community.independe.domain.member.Member;
+import community.independe.security.service.MemberContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
@@ -17,14 +19,17 @@ public abstract class SecuritySigner {
 
     protected String getJwtTokenInternal(MACSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
 
+        Member member = ((MemberContext) user).getMember();
+
         // jwk에서 알고리즘 정보와 keyId를 가져와 Header를 생성
         JWSHeader header = new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
         // Claim정보 생성
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject("user")
                 .issuer("http://localhost:8080")
-                .claim("username", user.getUsername()) // 사용자 Id
-                .claim("authority", user.getAuthorities()) // 사용자 권한
+                .claim("username", member.getUsername()) // 사용자 Id
+                .claim("authority", member.getRole()) // 사용자 권한
+                .claim("nickname", member.getNickname()) // 사용자 닉네임
                 .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 10)) // 10분
                 .build();
         SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
@@ -37,14 +42,17 @@ public abstract class SecuritySigner {
     protected String getTokenOAuth2(MACSigner jwsSigner, OAuth2User oAuth2User, JWK jwk) throws JOSEException {
 
         Map<String, Object> attributes = (Map<String, Object>) oAuth2User.getAttributes().get("response");
+        Member member = ((MemberContext) oAuth2User).getMember();
+
 
         JWSHeader header = new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject("user")
                 .issuer("http://localhost:8080")
-                .claim("username", attributes.get("email")) // 사용자 Id
-                .claim("authority", "ROLE_USER") // 사용자 권한
+                .claim("username", member.getUsername()) // 사용자 Id
+                .claim("authority", member.getRole()) // 사용자 권한
+                .claim("nickname", member.getNickname()) // 사용자 nickname
                 .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 10)) // 10분
                 .build();
         SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
