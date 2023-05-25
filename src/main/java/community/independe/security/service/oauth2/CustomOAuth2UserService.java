556723dd28fa9fbeb2e918d6831bb3fe34341ed3
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-25 14:35:15 +0900

    Refactor CustomOAuth2UserService add PasswordEncoder

--- src/main/java/community/independe/security/service/oauth2/CustomOAuth2UserService.java
+++ src/main/java/community/independe/security/service/oauth2/CustomOAuth2UserService.java
@@ -4,9 +4,13 @@ import community.independe.domain.member.Member;
 import community.independe.domain.member.oauth2.NaverUser;
 import community.independe.domain.member.oauth2.ProviderUser;
 import community.independe.repository.MemberRepository;
+import community.independe.security.service.MemberContext;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -15,6 +19,9 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Getter
 @Service
@@ -22,11 +29,10 @@ import org.springframework.stereotype.Service;
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
-        log.info("in oauth2 service");
 
         ClientRegistration clientRegistration = userRequest.getClientRegistration();
         OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
@@ -35,12 +41,16 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         ProviderUser providerUser = providerUser(clientRegistration, oAuth2User);
 
         // 회원가입
-        register(providerUser, userRequest);
+        Member savedMember = register(providerUser, userRequest);
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(savedMember.getRole()));
 
-        return oAuth2User;
+        MemberContext memberContext = new MemberContext(savedMember, roles, oAuth2User.getAttributes());
+
+        return memberContext;
     }
 
-    public void register(ProviderUser providerUser, OAuth2UserRequest userRequest) {
+    public Member register(ProviderUser providerUser, OAuth2UserRequest userRequest) {
 
         Member findMember = memberRepository.findByUsername(providerUser.getUsername());
 
@@ -49,16 +59,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             Member member = Member.builder()
                     .registrationId(providerUser.getId())
                     .username(providerUser.getUsername())
-                    .password(providerUser.getPassword())
+                    .password(passwordEncoder.encode(providerUser.getPassword()))
                     .email(providerUser.getEmail())
                     .role(providerUser.getAuthorities().get(0).toString())
                     .nickname(providerUser.getNickname())
                     .provider(providerUser.getProvider())
                     .build();
 
-            memberRepository.save(member);
+            Member savedMember = memberRepository.save(member);
+
+            return savedMember;
         } else {
-            throw new IllegalArgumentException("Member is exist");
+            return findMember;
         }
     }
 
