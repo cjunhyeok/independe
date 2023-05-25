3b98ca644b95492a24b34a419cd07503fe4f14f7
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-25 14:29:23 +0900

    Create Simple OAuth2AuthenticationFailureHandler

---
+++ src/main/java/community/independe/security/handler/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,16 @@
+package community.independe.security.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+     }
+}
