e9a2cc55826b24965949282c80d3a12918186f34
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-04 10:49:35 +0900

    Add nickname field in SearchResponse

--- src/main/java/community/independe/api/dtos/post/SearchResponse.java
+++ src/main/java/community/independe/api/dtos/post/SearchResponse.java
@@ -14,6 +14,7 @@ public class SearchResponse {
 
     private Long postId;
     private String title;
+    private String nickname;
     private String independentPostType;
     private String regionType;
     private String regionPostType;
