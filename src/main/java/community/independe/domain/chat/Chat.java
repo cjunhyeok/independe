d36f613eadcd6b5e838fc390e05ef191c64d3d0a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-06 14:04:45 +0900

    Create Chat Entity

---
+++ src/main/java/community/independe/domain/chat/Chat.java
@@ -0,0 +1,37 @@
+package community.independe.domain.chat;
+
+import community.independe.domain.BaseEntity;
+import community.independe.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "chat_id")
+    private Long id;
+    private String content; // 메시지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender; // 발신자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Member receiver; // 수신자
+    private Boolean isRead;
+
+
+    @Builder
+    public Chat(Member sender, Member receiver, String content, Boolean isRead) {
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.isRead = isRead;
+    }
+}
