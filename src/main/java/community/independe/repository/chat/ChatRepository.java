6bc53d3a768b880ef37f95320ed775c5212408e2
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-06 14:05:34 +0900

    Create ChatRepository with Jpa

---
+++ src/main/java/community/independe/repository/chat/ChatRepository.java
@@ -0,0 +1,22 @@
+package community.independe.repository.chat;
+
+import community.independe.domain.chat.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ChatRepository extends JpaRepository<Chat, Long>, ChatRepositoryCustom {
+
+    @Query(value = "select c from Chat c join fetch c.receiver" +
+            " join fetch c.sender" +
+            " where (c.sender.id = :loginMemberId and c.receiver.id = :receiverId)" +
+            " or (c.sender.id = :receiverId and c.receiver.id = :loginMemberId)",
+            countQuery = "select c from Chat c join fetch c.receiver" +
+                    " join fetch c.sender" +
+                    " where (c.sender.id = :loginMemberId and c.receiver.id = :receiverId)" +
+                    " or (c.sender.id = :receiverId and c.receiver.id = :loginMemberId)")
+    List<Chat> findChatHistory(@Param("loginMemberId") Long loginMemberId,
+                               @Param("receiverId") Long receiverId);
+}
