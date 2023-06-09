355d1ad3e8b56cb05028b9cbecdf5e336d7d0150
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-09 09:48:06 +0900

    Create DelegatingProviderUserConverter for providerUserConverters

---
+++ src/main/java/community/independe/converters/DelegatingProviderUserConverter.java
@@ -0,0 +1,38 @@
+package community.independe.converters;
+
+import community.independe.domain.member.oauth2.ProviderUser;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+public class DelegatingProviderUserConverter implements ProviderUserConverter<ProviderUserRequest, ProviderUser>{
+
+    private List<ProviderUserConverter<ProviderUserRequest, ProviderUser>> converters;
+
+    public DelegatingProviderUserConverter() {
+        List<ProviderUserConverter<ProviderUserRequest, ProviderUser>> providerUserConverters =
+                Arrays.asList(new OAuth2NaverProviderUserConverter(),
+                              new OAuth2KakaoProviderUserConverter());
+
+        this.converters = Collections.unmodifiableList(new LinkedList<>(providerUserConverters));
+    }
+
+    @Override
+    public ProviderUser converter(ProviderUserRequest providerUserRequest) {
+
+        Assert.notNull(providerUserRequest, "providerUserRequest cannot be null");
+
+        for (ProviderUserConverter<ProviderUserRequest, ProviderUser> converter : converters) {
+            ProviderUser providerUser = converter.converter(providerUserRequest);
+
+            if (providerUser != null) return providerUser;
+        }
+
+        return null;
+    }
+}
