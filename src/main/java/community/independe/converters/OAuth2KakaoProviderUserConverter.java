d87d61d1a2573477a44c1e0db665614ccac5e758
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-09 09:48:51 +0900

    Create OAuth2KakaoProviderUserConverter implements ProviderUserConverter

---
+++ src/main/java/community/independe/converters/OAuth2KakaoProviderUserConverter.java
@@ -0,0 +1,20 @@
+package community.independe.converters;
+
+import community.independe.domain.member.oauth2.KakaoUser;
+import community.independe.domain.member.oauth2.OAuth2Config;
+import community.independe.domain.member.oauth2.ProviderUser;
+import community.independe.util.OAuth2Utils;
+
+public class OAuth2KakaoProviderUserConverter implements ProviderUserConverter<ProviderUserRequest, ProviderUser> {
+    @Override
+    public ProviderUser converter(ProviderUserRequest providerUserRequest) {
+
+        if (!providerUserRequest.clientRegistration().getRegistrationId().equals(OAuth2Config.SocialType.KAKAO.getSocialName())) {
+            return null;
+        }
+
+        return new KakaoUser(OAuth2Utils.getOtherAttributes(providerUserRequest.oAuth2User(), "kakao_account", "profile"),
+                providerUserRequest.oAuth2User(),
+                providerUserRequest.clientRegistration());
+    }
+}
