52ab8fbfcd3e088d5eb50ebcb65c7d57c3998ffd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-12 20:51:29 +0900

    Add createdDate in ChatApiController with dto

--- src/main/java/community/independe/api/dtos/chat/Message.java
+++ src/main/java/community/independe/api/dtos/chat/Message.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class Message {
@@ -12,12 +14,14 @@ public class Message {
     private Long receiverId;
     private Long chatRoomId;
     private String senderNickname;
+    private LocalDateTime createdDate;
 
     @Builder
-    public Message(String message, Long receiverId, Long chatRoomId, String senderNickname) {
+    public Message(String message, Long receiverId, Long chatRoomId, String senderNickname, LocalDateTime createdDate) {
         this.message = message;
         this.receiverId = receiverId;
         this.chatRoomId = chatRoomId;
         this.senderNickname = senderNickname;
+        this.createdDate = createdDate;
     }
 }
