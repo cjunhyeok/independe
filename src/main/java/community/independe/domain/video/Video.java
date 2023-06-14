b55e874910eb987ecb8ba530d3a2ffd554caca79
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-14 15:49:42 +0900

    Add @Enumerated(EnumType.STRING) for IndependentPostType in Video Entity

--- src/main/java/community/independe/domain/video/Video.java
+++ src/main/java/community/independe/domain/video/Video.java
@@ -1,9 +1,7 @@
 package community.independe.domain.video;
 
 import community.independe.domain.post.enums.IndependentPostType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +19,7 @@ public class Video {
     private String videoMasterUrl; // 채널 주인 url
     private String materName; // 채널명
     private String videoTitle; // 영상 제목
+    @Enumerated(EnumType.STRING)
     private IndependentPostType independentPostType; // 카테고리
     private int views;
 
