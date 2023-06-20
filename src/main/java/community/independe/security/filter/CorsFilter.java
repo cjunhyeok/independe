0f6e23a2598cf97b586a84b87a3c39feda230d14
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-20 16:11:52 +0900

    Create CorsFilter for check cors Origin

---
+++ src/main/java/community/independe/security/filter/CorsFilter.java
@@ -0,0 +1,22 @@
+package community.independe.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String origin = request.getHeader("Origin");
+
+        log.info("Request from domain: " + origin);
+
+        filterChain.doFilter(request, response);
+    }
+}
