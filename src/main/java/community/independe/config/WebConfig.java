f3d6d6f297a581fbe34ec91e1877fe5fc40191d6
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-21 17:12:45 +0900

    Create LogInterceptor for check request url with config

---
+++ src/main/java/community/independe/config/WebConfig.java
@@ -0,0 +1,17 @@
+package community.independe.config;
+
+import community.independe.config.handler.LogInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
+    }
+}
