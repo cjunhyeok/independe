c542eb9e9ef4203031f8bcab25dc5a8cf8a16920
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-06-23 00:38:45 +0900

    Add findTopByChatRoomOrderByDateDesc in ChatRepositoryCustom

--- src/main/java/community/independe/repository/chat/ChatRepositoryCustom.java
+++ src/main/java/community/independe/repository/chat/ChatRepositoryCustom.java
@@ -1,11 +1,13 @@
 package community.independe.repository.chat;
 
 import community.independe.domain.chat.Chat;
-import community.independe.domain.member.Member;
+import community.independe.domain.chat.ChatRoom;
 
 import java.util.List;
 
 public interface ChatRepositoryCustom {
 
-    List<Chat> findChatRooms(Member member);
+    List<Chat> findChatRooms(Long memberId);
+
+    Chat findTopByChatRoomOrderByDateDesc(ChatRoom chatRoom);
 }
