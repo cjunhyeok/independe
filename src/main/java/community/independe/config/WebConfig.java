e7dcb2aed251e8c08f43d9e119f315b7532089f0
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-11 22:59:28 +0900

    delete @Component WebConfig not use bean

--- src/main/java/community/independe/config/WebConfig.java
+++ src/main/java/community/independe/config/WebConfig.java
@@ -1,11 +1,10 @@
 package community.independe.config;
 
 import community.independe.config.handler.LogInterceptor;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+//@Component
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
