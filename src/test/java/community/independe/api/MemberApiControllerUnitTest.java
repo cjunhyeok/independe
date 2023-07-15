20165acd9cb2fb2db4f9b66ebb5ea660900d8d17
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-15 17:46:59 +0900

    Add duplicateUsernameTest in MemberApiControllerUnitTest

--- src/test/java/community/independe/api/MemberApiControllerUnitTest.java
+++ src/test/java/community/independe/api/MemberApiControllerUnitTest.java
@@ -2,6 +2,8 @@ package community.independe.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import community.independe.api.dtos.member.CreateMemberRequest;
+import community.independe.api.dtos.member.DuplicateUsernameRequest;
+import community.independe.domain.member.Member;
 import community.independe.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,16 +36,41 @@ class MemberApiControllerUnitTest {
     @WithMockUser(username = "testUsername")
     void createMemberTest() throws Exception {
 
+        // given
         CreateMemberRequest createMemberRequest = new CreateMemberRequest();
         createMemberRequest.setUsername("testUsername");
         createMemberRequest.setPassword("Aasdf123!@");
         createMemberRequest.setNickname("nick12");
 
+        // when
         ResultActions perform = mockMvc.perform(post("/api/members/new")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(createMemberRequest))
                 .with(csrf()));
 
+        // then
+        perform.andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "testUsername")
+    void duplicateUsernameTest() throws Exception {
+
+        // given
+        DuplicateUsernameRequest duplicateUsernameRequest = new DuplicateUsernameRequest();
+        duplicateUsernameRequest.setUsername("testUsername");
+        Member mockMember = Member.builder().build();
+
+        // stub
+        when(memberService.findByUsername(duplicateUsernameRequest.getUsername())).thenReturn(mockMember);
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/api/members/username")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(duplicateUsernameRequest))
+                .with(csrf()));
+
+        // then
         perform.andExpect(status().isOk());
     }
 }
