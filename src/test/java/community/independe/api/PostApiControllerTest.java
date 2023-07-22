8d4898551b95c0d3b06184861805f2ba7fe58f10
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-22 13:21:20 +0900

    Add createIndependentPostTest in PostApiControllerTest

--- src/test/java/community/independe/api/PostApiControllerTest.java
+++ src/test/java/community/independe/api/PostApiControllerTest.java
@@ -2,14 +2,24 @@ package community.independe.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import community.independe.domain.member.Member;
+import community.independe.domain.post.enums.IndependentPostType;
 import community.independe.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
@@ -31,4 +41,26 @@ public class PostApiControllerTest {
 
         memberRepository.save(member);
     }
+
+    @Test
+    @WithUserDetails(value = "testUsername", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    void createIndependentPostTest() throws Exception {
+        // given
+        String title = "testTitle";
+        String content = "testContent";
+        IndependentPostType independentPostType = IndependentPostType.CLEAN;
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/api/posts/independent/new")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .param("title", title)
+                .param("content", content)
+                .param("independentPostType", independentPostType.name())
+                .with(csrf()));
+
+        // then
+        perform.andExpect(status().isOk());
+    }
+
+
 }
