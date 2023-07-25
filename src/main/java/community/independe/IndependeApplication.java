7f254e2336115067b05d1d0709e5a1663d301a84
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-25 21:32:28 +0900

    Refactor IndependeApplication BCryptPasswordEncoder

--- src/main/java/community/independe/IndependeApplication.java
+++ src/main/java/community/independe/IndependeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
@@ -18,7 +18,7 @@ public class IndependeApplication {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
