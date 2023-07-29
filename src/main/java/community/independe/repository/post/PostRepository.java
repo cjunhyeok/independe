33cf9cfd203191cbf0b2c5f572dbf88e78085d16
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-29 09:41:21 +0900

    delete unused methods in PostRepository

--- src/main/java/community/independe/repository/post/PostRepository.java
+++ src/main/java/community/independe/repository/post/PostRepository.java
@@ -1,14 +1,8 @@
 package community.independe.repository.post;
 
 import community.independe.domain.post.Post;
-import community.independe.domain.post.enums.IndependentPostType;
-import community.independe.domain.post.enums.RegionPostType;
-import community.independe.domain.post.enums.RegionType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -22,25 +16,4 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
             " where p.regionType IS NOT NULL" +
             " and p.regionPostType IS NOT NULL")
     List<Post> findAllRegionPosts();
-
-    @Query(value = "select p from Post p join fetch p.member",
-    countQuery = "select count(p) from Post p")
-    Page<Post> findAllPostsWithMember(Pageable pageable);
-
-    @Query(value = "select p from Post p join fetch p.member" +
-            " where p.independentPostType = :independentPostType",
-    countQuery = "select count(p) from Post p" +
-            " where p.independentPostType =: independentPostType")
-    Page<Post> findAllIndependentPostsByTypeWithMember(@Param("independentPostType") IndependentPostType independentPostType,
-                                                       Pageable pageable);
-
-    @Query(value = "select p from Post p join fetch p.member" +
-            " where p.regionType = :regionType" +
-            " and p.regionPostType = :regionPostType",
-    countQuery = "select count(p) from Post p" +
-            " where p.regionType = :regionType" +
-            " and p.regionPostType = :regionPostType")
-    Page<Post> findAllRegionPostsByTypesWithMember(@Param("regionType")RegionType regionType,
-                                                   @Param("regionPostType")RegionPostType regionPostType,
-                                                   Pageable pageable);
 }
