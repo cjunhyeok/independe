bb16feb58f124f1aaf819ced0076fa0047d038b2
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-23 10:42:22 +0900

    Create EmitterService with subscribe method

---
+++ src/main/java/community/independe/service/EmitterService.java
@@ -0,0 +1,8 @@
+package community.independe.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface EmitterService {
+
+    SseEmitter subscribe(Long memberId);
+}
