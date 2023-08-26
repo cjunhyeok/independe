9f3520715a67b04ddae7c486bc9a1e1a669260d1
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-26 18:51:44 +0900

    Create RefreshTokenRepository extends CrudRepository with findByRefreshToken method

---
+++ src/main/java/community/independe/repository/token/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package community.independe.repository.token;
+
+import community.independe.domain.token.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    RefreshToken findByRefreshToken(String refreshToken);
+}
