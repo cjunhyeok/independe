8f9972a8812da8d0f755979a3594fdd82270f1c8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-03 13:23:23 +0900

    Refactor JwtAuthorizationMacFilter for Exception Response

--- src/main/java/community/independe/security/filter/JwtAuthorizationMacFilter.java
+++ src/main/java/community/independe/security/filter/JwtAuthorizationMacFilter.java
@@ -1,14 +1,20 @@
 package community.independe.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import community.independe.security.exception.JwtNotFoundException;
+import community.independe.security.exception.JwtVerifyException;
 import community.independe.util.JwtTokenVerifier;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 // OncePerRequestFilter : 요청에 대해 한번만 실행
 @Slf4j
@@ -22,7 +28,27 @@ public class JwtAuthorizationMacFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        jwtTokenVerifier.verifyToken(request);
-        filterChain.doFilter(request, response);
+        try {
+            jwtTokenVerifier.verifyToken(request);
+        } catch (RuntimeException e) {
+            if (e instanceof JwtNotFoundException) {
+                makeExceptionMessage(response, "토큰을 찾을 수 없습니다.", e);
+            } else if (e instanceof JwtVerifyException) {
+                makeExceptionMessage(response, "토큰 검증에 실패하였습니다.", e);
+            } else {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+
+    private void makeExceptionMessage(HttpServletResponse response, String message, Exception e) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("message", message);
+        body.put("exceptionMessage", e.getMessage());
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
