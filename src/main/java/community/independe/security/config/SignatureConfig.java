c814ec3afdb43edbe75365ea8fc1e2c7a0f7d15a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-04 15:10:29 +0900

    Add MemberRepository Field in SignatureConfig

--- src/main/java/community/independe/security/config/SignatureConfig.java
+++ src/main/java/community/independe/security/config/SignatureConfig.java
@@ -4,16 +4,21 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import community.independe.repository.MemberRepository;
 import community.independe.security.signature.MacSecuritySigner;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SignatureConfig {
+
+    private final MemberRepository memberRepository;
 
     @Bean
     public MacSecuritySigner macSecuritySigner() {
-        return new MacSecuritySigner();
+        return new MacSecuritySigner(memberRepository);
     }
 
     @Bean
