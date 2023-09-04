0cae848c1381efb29c3340ec734a7cb469cd7ef4
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-04 15:12:04 +0900

    Add MemberRepository Field in MacSecuritySigner and Add getRefreshJwtToken

--- src/main/java/community/independe/security/signature/MacSecuritySigner.java
+++ src/main/java/community/independe/security/signature/MacSecuritySigner.java
@@ -4,22 +4,32 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
-import org.springframework.security.core.userdetails.UserDetails;
+import community.independe.repository.MemberRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 // 대칭키
 public class MacSecuritySigner extends SecuritySigner{
 
+    public MacSecuritySigner(MemberRepository memberRepository) {
+        super(memberRepository);
+    }
+
     @Override
-    public String getJwtToken(UserDetails user, JWK jwk) throws JOSEException {
+    public String getJwtToken(String username, JWK jwk) throws JOSEException {
         // 토큰 발행
         MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey()); // 시크릿 키를 이용해 Signer 생성
-        return super.getJwtTokenInternal(jwsSigner, user, jwk); // Token 발행은 공통이므로 부모에게 전달
+        return super.getJwtTokenInternal(jwsSigner, username, jwk); // Token 발행은 공통이므로 부모에게 전달
     }
 
     @Override
     public String getOAuth2JwtToken(OAuth2User oAuth2User, JWK jwk) throws JOSEException {
         MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey()); // 시크릿 키를 이용해 Signer 생성
         return super.getTokenOAuth2(jwsSigner, oAuth2User, jwk);
+    }
+
+    @Override
+    public String getRefreshJwtToken(String username, JWK jwk) throws JOSEException {
+        MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey()); // 시크릿 키를 이용해 Signer 생성
+        return super.getRefreshToken(jwsSigner, username, jwk);
     }
 }
