bf316a30d3d50b5799c20e2c32c2dc02de5ab14e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-04 15:11:29 +0900

    Add MemberRepository Field in SecuritySigner and Abstract getRefreshJwtToken

--- src/main/java/community/independe/security/signature/SecuritySigner.java
+++ src/main/java/community/independe/security/signature/SecuritySigner.java
@@ -5,12 +5,11 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import community.independe.domain.member.Member;
+import community.independe.repository.MemberRepository;
 import community.independe.security.service.MemberContext;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Date;
@@ -18,9 +17,15 @@ import java.util.Map;
 
 public abstract class SecuritySigner {
 
-    protected String getJwtTokenInternal(MACSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
+    private final MemberRepository memberRepository;
 
-        Member member = ((MemberContext) user).getMember();
+    public SecuritySigner(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    protected String getJwtTokenInternal(MACSigner jwsSigner, String username, JWK jwk) throws JOSEException {
+
+        Member findMember = memberRepository.findByUsername(username);
 
         // jwk에서 알고리즘 정보와 keyId를 가져와 Header를 생성
         JWSHeader header = new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
@@ -28,10 +33,10 @@ public abstract class SecuritySigner {
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject("user")
                 .issuer("http://spring:8080")
-                .claim("username", member.getUsername())
-                .claim("authority", member.getRole()) // 사용자 권한
-                .claim("nickname", member.getNickname()) // 사용자 닉네임
-                .claim("region", member.getRegion()) // 사용자 지
+                .claim("username", findMember.getUsername())
+                .claim("authority", findMember.getRole()) // 사용자 권한
+                .claim("nickname", findMember.getNickname()) // 사용자 닉네임
+                .claim("region", findMember.getRegion()) // 사용자 지
                 .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 10)) // 10분
                 .build();
         SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
@@ -64,10 +69,9 @@ public abstract class SecuritySigner {
         return jwtToken;
     }
 
-    public String getRefreshToken(UserDetails userDetails, JWK jwk) throws JOSEException {
-        MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey()); // 시크릿 키를 이용해 Signer 생성
+    protected String getRefreshToken(MACSigner jwsSigner, String username, JWK jwk) throws JOSEException {
 
-        Member member = ((MemberContext) userDetails).getMember();
+        Member findMember = memberRepository.findByUsername(username);
 
         // jwk에서 알고리즘 정보와 keyId를 가져와 Header를 생성
         JWSHeader header = new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
@@ -75,10 +79,10 @@ public abstract class SecuritySigner {
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject("user")
                 .issuer("http://localhost:8080")
-                .claim("username", member.getUsername())
-                .claim("authority", member.getRole()) // 사용자 권한
-                .claim("nickname", member.getNickname()) // 사용자 닉네임
-                .claim("region", member.getRegion()) // 사용자 지역
+                .claim("username", findMember.getUsername())
+                .claim("authority", findMember.getRole()) // 사용자 권한
+                .claim("nickname", findMember.getNickname()) // 사용자 닉네임
+                .claim("region", findMember.getRegion()) // 사용자 지역
                 .expirationTime(new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000))) // 1주일
                 .build();
         SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
@@ -88,7 +92,9 @@ public abstract class SecuritySigner {
         return jwtToken;
     }
 
-    public abstract String getJwtToken(UserDetails user, JWK jwk) throws JOSEException;
+    public abstract String getJwtToken(String username, JWK jwk) throws JOSEException;
 
     public abstract String getOAuth2JwtToken(OAuth2User oAuth2User, JWK jwk) throws JOSEException;
+
+    public abstract String getRefreshJwtToken(String username, JWK jwk) throws JOSEException;
 }
