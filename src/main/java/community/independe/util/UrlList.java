12ceb658ca10b40e6406af383ef5d1c16630ed13
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-06 18:45:47 +0900

    Create UrlList for white and black list

---
+++ src/main/java/community/independe/util/UrlList.java
@@ -0,0 +1,37 @@
+package community.independe.util;
+
+public abstract class UrlList {
+
+    private static final String[] whiteList = {"/",
+            "/actuator/**",
+            "/ws",
+            "/ws/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/oauth2/**",
+            "/api/login",
+            "/api/members/new",
+            "/api/members/username",
+            "/api/members/nickname",
+            "/api/posts/main",
+            "/api/posts/**",
+            "/api/refreshToken"};
+
+    private static final String[] blackList = {
+            "/api/posts/new",
+            "/api/oauth/members",
+            "/api/posts/region/new",
+            "/api/posts/independent/new",
+            "/api/members/region",
+            "/api/posts/update",
+            "/api/chat/**"
+    };
+
+    public static String[] getWhiteList() {
+        return whiteList;
+    }
+
+    public static String[] getBlackList() {
+        return blackList;
+    }
+}
