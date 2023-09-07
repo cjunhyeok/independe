ed1bc358d628c446d0d525f33d480c4884da1870
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-07 15:12:56 +0900

    Add findAllByMemberId in AlarmRepository

--- src/main/java/community/independe/repository/alarm/AlarmRepository.java
+++ src/main/java/community/independe/repository/alarm/AlarmRepository.java
@@ -2,6 +2,17 @@ package community.independe.repository.alarm;
 
 import community.independe.domain.alarm.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+
+    @Query(value = "select a from Alarm a left join fetch a.member" +
+            " where a.member.id = :memberId" +
+            " order by a.createdDate asc",
+    countQuery = "select a from Alarm a" +
+            " where a.member.id = :memberId" +
+            " order by a.createdDate asc")
+    List<Alarm> findAllByMemberId(Long memberId);
 }
