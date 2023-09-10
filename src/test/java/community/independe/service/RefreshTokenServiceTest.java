3ccdb734abe4f3e198b6c68eee7a12700b4b5dcc
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-10 16:14:58 +0900

    Refactor RefreshTokenService.save method Parameter

--- src/test/java/community/independe/service/RefreshTokenServiceTest.java
+++ src/test/java/community/independe/service/RefreshTokenServiceTest.java
@@ -7,12 +7,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.GrantedAuthority;
 
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class RefreshTokenServiceTest {
@@ -26,17 +25,19 @@ public class RefreshTokenServiceTest {
     void saveTest() {
         // given
         String ip = "mockIp";
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        String username = "username";
+        Set<String> authorities = new HashSet<>();
         String token = "mockRefreshToken";
         RefreshToken refreshToken = RefreshToken.builder()
                 .ip(ip)
                 .authorities(authorities)
+                .username(username)
                 .refreshToken(token)
                 .build();
 
         // when
         when(refreshTokenRepository.save(any(RefreshToken.class))).thenReturn(refreshToken);
-        refreshTokenService.save(ip, authorities, token);
+        refreshTokenService.save(ip, authorities, token, username);
 
         // then
         verify(refreshTokenRepository, times(1)).save(any(RefreshToken.class));
