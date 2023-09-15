e8d3c96196e896b1c8d5948994743635faa6b8e4
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-15 16:36:32 +0900

    Add findAllByMemberIdTest

--- src/test/java/community/independe/service/AlarmServiceTest.java
+++ src/test/java/community/independe/service/AlarmServiceTest.java
@@ -5,12 +5,14 @@ import community.independe.domain.alarm.AlarmType;
 import community.independe.domain.member.Member;
 import community.independe.repository.MemberRepository;
 import community.independe.repository.alarm.AlarmRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -45,5 +47,21 @@ public class AlarmServiceTest {
         // then
         verify(memberRepository).findById(memberId);
         verify(alarmRepository).save(any(Alarm.class));
+    }
+
+    @Test
+    void findAllByMemberIdTest() {
+        // given
+        Long memberId = 1L;
+
+        // stub
+        when(alarmRepository.findAllByMemberId(memberId)).thenReturn(List.of());
+
+        // when
+        List<Alarm> findAlarms = alarmService.findAllByMemberId(memberId);
+
+        // then
+        verify(alarmRepository).findAllByMemberId(memberId);
+        Assertions.assertThat(findAlarms).isNotNull();
     }
 }
