ee68cb6cbe4bb3a845afecdb6664263428d7d806
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-17 16:10:29 +0900

    Add findByTitleTest with initData in ChatRoomRepositoryTest

--- src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
@@ -3,6 +3,7 @@ package community.independe.repository;
 import community.independe.domain.chat.ChatRoom;
 import community.independe.domain.member.Member;
 import community.independe.repository.chat.ChatRoomRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,6 +17,23 @@ class ChatRoomRepositoryTest {
     private ChatRoomRepository chatRoomRepository;
     @Autowired
     private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void initData() {
+        Member sender = Member.builder().username("sender").build();
+        memberRepository.save(sender);
+        Member receiver = Member.builder().username("receiver").build();
+        memberRepository.save(receiver);
+        String title = "initSenderToReceiver";
+
+        ChatRoom chatRoom = ChatRoom
+                .builder()
+                .title(title)
+                .sender(sender)
+                .receiver(receiver)
+                .build();
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+    }
 
     @Test
     void saveTest() {
@@ -40,5 +58,19 @@ class ChatRoomRepositoryTest {
         assertThat(savedChatRoom.getSender()).isEqualTo(sender);
         assertThat(savedChatRoom.getReceiver()).isEqualTo(receiver);
         assertThat(savedChatRoom.getTitle()).isEqualTo(title);
+    }
+
+    @Test
+    void findByTitleTest() {
+        // given
+        String title = "initSenderToReceiver";
+
+        // when
+        ChatRoom findChatRoom = chatRoomRepository.findByTitle(title);
+
+        // then
+        assertThat(findChatRoom.getTitle()).isEqualTo(title);
+        assertThat(findChatRoom.getSender().getUsername()).isEqualTo("sender");
+        assertThat(findChatRoom.getReceiver().getUsername()).isEqualTo("receiver");
     }
 }
