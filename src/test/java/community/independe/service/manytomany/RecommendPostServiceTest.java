7f8f441c1e383e7582319f867e36e715f3489c6d
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-18 15:47:18 +0900

    Create RecommendPostServiceTest with saveTest

---
+++ src/test/java/community/independe/service/manytomany/RecommendPostServiceTest.java
@@ -0,0 +1,54 @@
+package community.independe.service.manytomany;
+
+import community.independe.domain.manytomany.RecommendPost;
+import community.independe.domain.member.Member;
+import community.independe.domain.post.Post;
+import community.independe.repository.MemberRepository;
+import community.independe.repository.manytomany.RecommendPostRepository;
+import community.independe.repository.post.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RecommendPostServiceTest {
+
+    @InjectMocks
+    private RecommendPostServiceImpl recommendPostService;
+    @Mock
+    private RecommendPostRepository recommendPostRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PostRepository postRepository;
+
+    @Test
+    void saveTest() {
+        // given
+        Long postId = 1L;
+        Long memberId = 1L;
+        Member mockMember = Member.builder().build();
+        Post mockPost = Post.builder().member(mockMember).build();
+        RecommendPost mockRecommendPost = RecommendPost.builder().post(mockPost).member(mockMember).build();
+
+        // stub
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(mockMember));
+        when(recommendPostRepository.save(any(RecommendPost.class))).thenReturn(mockRecommendPost);
+
+        // when
+        recommendPostService.save(postId, memberId);
+
+        // then
+        verify(postRepository, times(1)).findById(postId);
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(recommendPostRepository, times(1)).save(any(RecommendPost.class));
+    }
+}
