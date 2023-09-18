df72cc66944d6252713099487928fbac4e4f52b0
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-18 16:00:47 +0900

    Create ReportPostRepositoryTest with saveTest

---
+++ src/test/java/community/independe/repository/manytomany/ReportPostRepositoryTest.java
@@ -0,0 +1,40 @@
+package community.independe.repository.manytomany;
+
+import community.independe.domain.manytomany.ReportPost;
+import community.independe.domain.member.Member;
+import community.independe.domain.post.Post;
+import community.independe.repository.MemberRepository;
+import community.independe.repository.post.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class ReportPostRepositoryTest {
+
+    @Autowired
+    private ReportPostRepository reportPostRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void saveTest() {
+        // given
+        Member member = Member.builder().build();
+        Member savedMember = memberRepository.save(member);
+        Post post = Post.builder().member(member).build();
+        Post savedPost = postRepository.save(post);
+        ReportPost reportPost =
+                ReportPost.builder().member(savedMember).post(savedPost).isReport(true).build();
+
+        // when
+        ReportPost savedReportPost = reportPostRepository.save(reportPost);
+
+        // then
+        assertThat(savedReportPost).isEqualTo(reportPost);
+    }
+}
