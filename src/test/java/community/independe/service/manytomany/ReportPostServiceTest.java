7203e2aa3d67b50541236ac9dc39026722336021
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-18 16:12:06 +0900

    Create ReportPostServiceTest with saveTest

---
+++ src/test/java/community/independe/service/manytomany/ReportPostServiceTest.java
@@ -0,0 +1,55 @@
+package community.independe.service.manytomany;
+
+import community.independe.domain.manytomany.ReportPost;
+import community.independe.domain.member.Member;
+import community.independe.domain.post.Post;
+import community.independe.repository.MemberRepository;
+import community.independe.repository.manytomany.ReportPostRepository;
+import community.independe.repository.post.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ReportPostServiceTest {
+
+    @InjectMocks
+    private ReportPostServiceImpl reportPostService;
+    @Mock
+    private ReportPostRepository reportPostRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PostRepository postRepository;
+
+    @Test
+    void saveTest() {
+        // given
+        Long postId = 1L;
+        Long memberId = 1L;
+        Member mockMember = Member.builder().build();
+        Post mockPost = Post.builder().member(mockMember).build();
+        ReportPost mockReportPost =
+                ReportPost.builder().member(mockMember).post(mockPost).build();
+
+        // stub
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(mockMember));
+        when(reportPostRepository.save(any(ReportPost.class))).thenReturn(mockReportPost);
+
+        // when
+        reportPostService.save(postId, memberId);
+
+        // then
+        verify(postRepository, times(1)).findById(postId);
+        verify(memberRepository, times(1)).findById(memberId);
+        verify(reportPostRepository, times(1)).save(any(ReportPost.class));
+    }
+}
