06596e3ca857ed8a70ff0482aad944cd96b5f8bc
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-18 16:14:10 +0900

    Add updateIsReportTest in ReportPostServiceTest

--- src/test/java/community/independe/service/manytomany/ReportPostServiceTest.java
+++ src/test/java/community/independe/service/manytomany/ReportPostServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -51,5 +52,19 @@ public class ReportPostServiceTest {
         verify(postRepository, times(1)).findById(postId);
         verify(memberRepository, times(1)).findById(memberId);
         verify(reportPostRepository, times(1)).save(any(ReportPost.class));
+    }
+
+    @Test
+    void updateIsReportTest() {
+        // given
+        ReportPost mockReportPost =
+                ReportPost.builder().isReport(true).build();
+        Boolean isReport = false;
+
+        // when
+        reportPostService.updateIsReport(mockReportPost, isReport);
+
+        // then
+        assertThat(mockReportPost.getIsReport()).isEqualTo(isReport);
     }
 }
