b83fb935b34fce6a40a344fe1143acec3a5328c1
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-20 15:37:30 +0900

    Add saveFailTests in RecommendCommentServiceTest

--- src/test/java/community/independe/service/manytomany/RecommendCommentServiceTest.java
+++ src/test/java/community/independe/service/manytomany/RecommendCommentServiceTest.java
@@ -3,6 +3,8 @@ package community.independe.service.manytomany;
 import community.independe.domain.comment.Comment;
 import community.independe.domain.manytomany.RecommendComment;
 import community.independe.domain.member.Member;
+import community.independe.exception.notfound.CommentNotFountException;
+import community.independe.exception.notfound.MemberNotFountException;
 import community.independe.repository.MemberRepository;
 import community.independe.repository.comment.CommentRepository;
 import community.independe.repository.manytomany.RecommendCommentRepository;
@@ -48,6 +50,44 @@ public class RecommendCommentServiceTest {
         verify(commentRepository, times(1)).findById(memberId);
         verify(memberRepository, times(1)).findById(memberId);
         verify(recommendCommentRepository, times(1)).save(any(RecommendComment.class));
+    }
+
+    @Test
+    void saveCommentFailTest() {
+        // given
+        Long commentId = 1L;
+        Long memberId = 1L;
+
+        // stub
+        when(commentRepository.findById(commentId)).thenReturn(Optional.empty());
+
+        // when
+        assertThatThrownBy(() -> recommendCommentService.save(commentId, memberId))
+                .isInstanceOf(CommentNotFountException.class)
+                .hasMessage("Comment Not Exist");
+
+        // then
+        verify(commentRepository, times(1)).findById(commentId);
+    }
+
+    @Test
+    void saveMemberFailTest() {
+        // given
+        Long commentId = 1L;
+        Long memberId = 1L;
+
+        // stub
+        when(commentRepository.findById(commentId)).thenReturn(Optional.of(Comment.builder().build()));
+        when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        // when
+        assertThatThrownBy(() -> recommendCommentService.save(commentId, memberId))
+                .isInstanceOf(MemberNotFountException.class)
+                .hasMessage("Member Not Exist");
+
+        // then
+        verify(commentRepository, times(1)).findById(commentId);
+        verify(memberRepository, times(1)).findById(memberId);
     }
 
     @Test
