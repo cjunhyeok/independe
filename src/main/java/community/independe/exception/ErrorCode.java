64b2e40d18abe3a5cd5411c3416909d374d86033
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-27 12:02:20 +0900

    Create ErrorCode for Exception

---
+++ src/main/java/community/independe/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package community.independe.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "회원이 존재하지 않습니다."),
+    MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 아이디입니다."),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 access token 입니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 refresh token 입니다."),
+    REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "잘못된 refresh token 요청 입니다.");
+
+    private final HttpStatus status;
+    private final String errorMessage;
+}
