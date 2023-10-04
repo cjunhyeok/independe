1940bb923b99f3275abdedbcefb29997427e4488
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-04 19:49:37 +0900

    Add COMMENT, POST NOT FOUND

--- src/main/java/community/independe/exception/ErrorCode.java
+++ src/main/java/community/independe/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 아이디입니다."),
     ACCESS_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 access token 입니다."),
     REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 refresh token 입니다."),
-    REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "잘못된 refresh token 요청 입니다.");
+    REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "잘못된 refresh token 요청 입니다."),
+    COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "댓글이 존재하지 않습니다"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String errorMessage;
