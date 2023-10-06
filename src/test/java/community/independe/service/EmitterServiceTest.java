9e7b515b941b2e1aa67085128241fd1753888184
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-06 15:29:47 +0900

    Add subscribeFindFailTest

--- src/test/java/community/independe/service/EmitterServiceTest.java
+++ src/test/java/community/independe/service/EmitterServiceTest.java
@@ -1,6 +1,7 @@
 package community.independe.service;
 
 import community.independe.repository.emitter.EmitterRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,8 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class EmitterServiceTest {
@@ -26,12 +26,34 @@ public class EmitterServiceTest {
         SseEmitter sseEmitter = new SseEmitter();
 
         // stub
-        emitterRepository.save(memberId, sseEmitter);
+        doNothing().when(emitterRepository).save(anyLong(), any(SseEmitter.class));
+        when(emitterRepository.findById(memberId)).thenReturn(sseEmitter);
 
         // when
-        emitterService.subscribe(memberId);
+        SseEmitter subscribe = emitterService.subscribe(memberId);
 
         // then
-        verify(emitterRepository, times(1)).save(memberId, sseEmitter);
+        verify(emitterRepository, times(1)).save(anyLong(), any(SseEmitter.class));
+        verify(emitterRepository, times(1)).findById(memberId);
+        Assertions.assertThat(subscribe).isExactlyInstanceOf(SseEmitter.class);
+    }
+
+    @Test
+    void subscribeFindFailTest() {
+        // given
+        Long memberId = 1L;
+        SseEmitter sseEmitter = new SseEmitter();
+
+        // stub
+        doNothing().when(emitterRepository).save(anyLong(), any(SseEmitter.class));
+        when(emitterRepository.findById(memberId)).thenReturn(null);
+
+        // when
+        SseEmitter subscribe = emitterService.subscribe(memberId);
+
+        // then
+        verify(emitterRepository, times(1)).save(anyLong(), any(SseEmitter.class));
+        verify(emitterRepository, times(1)).findById(memberId);
+        verifyNoMoreInteractions(emitterRepository);
     }
 }
