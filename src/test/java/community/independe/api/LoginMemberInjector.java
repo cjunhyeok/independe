54a4b97ec4847facb01c3a618f6f874c8c40f10b
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-09 15:26:17 +0900

    Create LoginMemberInjector for test access and refresh Token

---
+++ src/test/java/community/independe/api/LoginMemberInjector.java
@@ -0,0 +1,49 @@
+package community.independe.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Map;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@Component
+public class LoginMemberInjector {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private String refreshToken;
+    private String accessToken;
+
+    public void makeAccessAndRefreshToken() throws Exception {
+        String username = "testUsername";
+        String password = "testPasswrod1!";
+
+        ResultActions perform = mockMvc.perform(post("/api/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(Map.of(
+                        "username", username,
+                        "password", password
+                )))
+                .with(csrf()));
+
+        Cookie refreshTokenCookie = perform.andReturn().getResponse().getCookie("refreshToken");
+        refreshToken = refreshTokenCookie.getValue();
+        accessToken = perform.andReturn().getResponse().getHeader("Authorization");
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
