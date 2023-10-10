ee68c07ccac1d7c71ea6c6f8e24ec87ae2163e7b
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-10 14:22:58 +0900

    Refactor jwt header for login in PostApiControllerTest

--- src/test/java/community/independe/api/PostApiControllerTest.java
+++ src/test/java/community/independe/api/PostApiControllerTest.java
@@ -17,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -43,14 +41,20 @@ public class PostApiControllerTest {
     @Autowired
     private CommentService commentService;
     @Autowired
+    private LoginMemberInjector injector;
+    @Autowired
     private PlatformTransactionManager transactionManager;
     private TransactionStatus transactionStatus;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private String accessToken;
+    private String refreshToken;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws Exception {
         transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        memberService.join("username", "testPass1!", "testNick", null, null);
+        injector.makeAccessAndRefreshToken();
+        accessToken = injector.getAccessToken();
+        refreshToken = injector.getRefreshToken();
     }
 
     @AfterEach
@@ -61,7 +65,7 @@ public class PostApiControllerTest {
     @Test
     void independentPostsTest() throws Exception {
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedIndependentPost = postService.createIndependentPost(testUser.getId(),
                 "testTitle",
                 "testContent",
@@ -75,7 +79,7 @@ public class PostApiControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].postId").value(savedIndependentPost))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].nickName").value("testNick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].nickName").value("testNickname"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].title").value("testTitle"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].views").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.postsResponses[0].recommendCount").value(0))
@@ -85,7 +89,6 @@ public class PostApiControllerTest {
     }
 
     @Test
-    @WithUserDetails(value = "username", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void createIndependentPostTest() throws Exception {
         // given
         String title = "testTitle";
@@ -98,6 +101,7 @@ public class PostApiControllerTest {
                 .param("title", title)
                 .param("content", content)
                 .param("independentPostType", independentPostType.name())
+                .header("Authorization", accessToken)
                 .with(csrf()));
 
         // then
@@ -108,7 +112,7 @@ public class PostApiControllerTest {
     void regionPostsTest() throws Exception {
 
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedRegionPostId = postService.createRegionPost(
                 testUser.getId(),
                 "regionTitle",
@@ -124,7 +128,7 @@ public class PostApiControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].postId").value(savedRegionPostId))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].nickName").value("testNick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].nickName").value("testNickname"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].title").value("regionTitle"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].views").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].recommendCount").value(0))
@@ -134,7 +138,6 @@ public class PostApiControllerTest {
     }
 
     @Test
-    @WithUserDetails(value = "username", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void createRegionPostTest() throws Exception {
         // given
         String title = "regionTitle";
@@ -148,17 +151,17 @@ public class PostApiControllerTest {
                 .param("title", title)
                 .param("content", content)
                 .param("regionType", regionType.name())
-                .param("regionPostType", regionPostType.name()));
+                .param("regionPostType", regionPostType.name())
+                .header("Authorization", accessToken));
 
         // then
         perform.andExpect(status().isOk());
     }
 
     @Test
-    @WithUserDetails(value = "username", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void updatePostTest() throws Exception {
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedIndependentPostId = postService.createIndependentPost(
                 testUser.getId(),
                 "title",
@@ -172,7 +175,8 @@ public class PostApiControllerTest {
         ResultActions perform = mockMvc.perform(put("/api/posts/" + savedIndependentPostId)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .param("title", updateTitle)
-                .param("content", updateContent));
+                .param("content", updateContent)
+                .header("Authorization", accessToken));
         Post updatePost = postService.findById(savedIndependentPostId);
 
         // then
@@ -182,10 +186,10 @@ public class PostApiControllerTest {
     }
 
     @Test
-    @WithUserDetails(value = "username", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void deletePostTest() throws Exception {
+        // todo
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedIndependentPostId = postService.createIndependentPost(
                 testUser.getId(),
                 "title",
@@ -193,19 +197,18 @@ public class PostApiControllerTest {
                 IndependentPostType.COOK);
 
         // when
-        ResultActions perform = mockMvc.perform(delete("/api/posts/" + savedIndependentPostId));
+        ResultActions perform = mockMvc.perform(delete("/api/posts/" + savedIndependentPostId)
+                .header("Authorization", accessToken));
 
         // then
         perform.andExpect(status().isOk());
         Post post = postService.findById(savedIndependentPostId);
-//        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class,
-//                () -> postService.findById(savedIndependentPostId));
     }
 
     @Test
     void postTest() throws Exception {
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedIndependentPostId = postService.createIndependentPost(
                 testUser.getId(),
                 "title",
@@ -221,7 +224,7 @@ public class PostApiControllerTest {
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value("title"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.content").value("content"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname").value("testNick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname").value("testNickname"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.independentPostType").value(IndependentPostType.COOK.getDescription()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.regionType").isEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.regionPostType").isEmpty())
@@ -236,7 +239,7 @@ public class PostApiControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.isReport").value(false))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.bestComment").isEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].commentId").value(savedCommentId))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].nickname").value("testNick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].nickname").value("testNickname"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].content").value("comment"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].recommendCount").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.comments[0].parentId").isEmpty())
@@ -247,7 +250,7 @@ public class PostApiControllerTest {
     @Test
     void searchPostTest() throws Exception {
         // given
-        Member testUser = memberService.findByUsername("username");
+        Member testUser = memberService.findByUsername("testUsername");
         Long savedIndependentPostId = postService.createIndependentPost(
                 testUser.getId(),
                 "title",
@@ -266,7 +269,7 @@ public class PostApiControllerTest {
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].postId").value(savedIndependentPostId))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].title").value("title"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].nickname").value("testNick"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].nickname").value("testNickname"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].independentPostType").value(IndependentPostType.COOK.getDescription()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].regionType").isEmpty())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].regionPostType").isEmpty())
