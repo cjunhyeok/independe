50ca60635ab56554fb80425d42046565f8f363f7
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-12 15:40:57 +0900

    Create S3FilesServiceTest with saveFilesTest

---
+++ src/test/java/community/independe/service/S3FilesServiceTest.java
@@ -0,0 +1,59 @@
+package community.independe.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import community.independe.domain.file.Files;
+import community.independe.domain.post.Post;
+import community.independe.repository.file.FilesRepository;
+import community.independe.repository.post.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class S3FilesServiceTest {
+
+    @InjectMocks
+    private S3FilesServiceImpl filesService;
+    @Mock
+    private FilesRepository filesRepository;
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private AmazonS3Client amazonS3Client;
+
+    @Test
+    void saveFilesTest() throws IOException {
+        // given
+        Long postId = 1L;
+        List<MultipartFile> multipartFiles = new ArrayList<>();
+        multipartFiles.add(new MockMultipartFile("firstFile", "firstFileContent".getBytes()));
+        multipartFiles.add(new MockMultipartFile("secondFile", "secondFileContent".getBytes()));
+
+        // stub
+        when(postRepository.findById(postId)).thenReturn(Optional.of(Post.builder().build()));
+        when(amazonS3Client.getUrl(any(), any())).thenReturn(new URL("https://example-image.jpg"));
+        when(amazonS3Client.putObject(any())).thenReturn(any());
+
+        // when
+        List<Files> files = filesService.saveFiles(multipartFiles, postId);
+
+        // then
+        verify(postRepository, times(1)).findById(postId);
+        verify(amazonS3Client, times(2)).putObject(any(PutObjectRequest.class)); // s3 업로드 횟수 확인
+        verify(amazonS3Client, times(2)).getUrl(any(), any()); // s3 조회 횟수 확인
+    }
+}
