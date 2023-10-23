eaba7e0a8812ec3e552251b2e97eb0dd31f8bee3
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-23 14:35:50 +0900

    Add alarm and notify logic

--- src/main/java/community/independe/api/ChatApiController.java
+++ src/main/java/community/independe/api/ChatApiController.java
@@ -1,8 +1,11 @@
 package community.independe.api;
 
 import community.independe.api.dtos.chat.Message;
+import community.independe.domain.alarm.AlarmType;
 import community.independe.domain.member.Member;
 import community.independe.security.service.MemberContext;
+import community.independe.service.AlarmService;
+import community.independe.service.EmitterService;
 import community.independe.service.chat.ChatService;
 import community.independe.util.JwtTokenVerifier;
 import lombok.RequiredArgsConstructor;
@@ -21,16 +24,18 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class ChatApiController {
 
+    private final static String CHAT_MESSAGE = "채팅이 도착했습니다.";
     private final ChatService chatService;
     private final SimpMessagingTemplate simpMessagingTemplate; // 특정 상대에게 메시지를 보내기 위한 객체
     private final JwtTokenVerifier jwtTokenVerifier;
+    private final EmitterService emitterService;
+    private final AlarmService alarmService;
 
     @MessageMapping("/private-message")
     public Message receivePrivateMessage(@Payload Message message, @Header(name = "Authorization") String header){
 
-        if (!jwtTokenVerifier.verifyToken(header)) {
-            throw new RuntimeException();
-        }
+        jwtTokenVerifier.verifyToken(header);
+
         Member loginMember = ((MemberContext) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getMember();
 
         message.setSenderNickname(loginMember.getNickname());
@@ -38,6 +43,9 @@ public class ChatApiController {
 
         simpMessagingTemplate.convertAndSendToUser(message.getChatRoomId().toString(),"/private",message);
         Long savedChat = chatService.saveChat(loginMember.getId(), message.getReceiverId(), message.getMessage(), false);
+
+        emitterService.notify(message.getReceiverId(), CHAT_MESSAGE);
+        alarmService.saveAlarm(CHAT_MESSAGE, false, AlarmType.TALK, message.getReceiverId());
 
         return message;
     }
