7734f12f9884bf661ed17ed9e30ffa7b17c2d2a4
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-21 20:24:16 +0900

    Test: 내 알람 조회 api 테스트 개발

---
+++ src/test/java/community/independe/api/AlarmApiControllerTest.java
@@ -0,0 +1,56 @@
+package community.independe.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AlarmApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private LoginMemberInjector injector;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+    private TransactionStatus transactionStatus;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private String accessToken;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        injector.makeAccessAndRefreshToken();
+        accessToken = injector.getAccessToken();
+    }
+
+    @AfterEach
+    void afterTest() {
+        transactionManager.rollback(transactionStatus);
+    }
+
+    @Test
+    void alarmListTest() throws Exception {
+        // given
+
+        // when
+        ResultActions perform = mockMvc.perform(get("/api/alarms")
+                .header("Authorization", accessToken));
+
+        // then
+        perform.andExpect(status().isOk());
+    }
+}
