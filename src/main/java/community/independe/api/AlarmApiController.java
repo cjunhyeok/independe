2bc055bb08ad73b7cfc89c458f96bd9840bb5617
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-21 20:23:39 +0900

    Feat: @Controller 로 돼있던 오류 해결

--- src/main/java/community/independe/api/AlarmApiController.java
+++ src/main/java/community/independe/api/AlarmApiController.java
@@ -11,14 +11,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class AlarmApiController {
 
