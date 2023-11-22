351b88750fab2772c887830d753843456cb074ff
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-22 19:10:09 +0900

    Feat: 채팅방 서비스 레이어 개발

---
+++ src/main/java/community/independe/service/chat/ChatRoomService.java
@@ -0,0 +1,6 @@
+package community.independe.service.chat;
+
+public interface ChatRoomService {
+
+    Long saveChatRoom(Long senderId, Long receiverId);
+}
---
+++ src/main/java/community/independe/service/chat/ChatRoomServiceImpl.java
@@ -0,0 +1,42 @@
+package community.independe.service.chat;
+
+import community.independe.domain.chat.ChatRoom;
+import community.independe.domain.member.Member;
+import community.independe.exception.CustomException;
+import community.independe.exception.ErrorCode;
+import community.independe.repository.MemberRepository;
+import community.independe.repository.chat.ChatRoomRepository;
+import community.independe.util.SortedStringEditor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChatRoomServiceImpl implements ChatRoomService{
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public Long saveChatRoom(Long senderId, Long receiverId) {
+        Member findSender = memberRepository.findById(senderId).orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        Member findReceiver = memberRepository.findById(receiverId).orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        ChatRoom chatRoom = ChatRoom.builder()
+                .senderAndReceiver(SortedStringEditor.createSortedString(findSender.getId(), findReceiver.getId()))
+                .build();
+
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        return savedChatRoom.getId();
+    }
+}
