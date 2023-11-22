2bb0fcdaf878d2d664f3da1a5435cd72ed122ba7
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-22 15:31:52 +0900

    Feat: 채팅 리포지토리 개발

---
+++ src/main/java/community/independe/repository/chat/ChatRepository.java
@@ -0,0 +1,7 @@
+package community.independe.repository.chat;
+
+import community.independe.domain.chat.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+}
