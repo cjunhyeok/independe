a5736e2cdda22dcd9e30ab637d1063fd5eb92ccb
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-22 15:34:29 +0900

    Feat: 채팅 api saveChat 호출 파라미터 변경

--- src/main/java/community/independe/api/ChatApiController.java
+++ src/main/java/community/independe/api/ChatApiController.java
@@ -42,7 +42,7 @@ public class ChatApiController {
         message.setCreatedDate(LocalDateTime.now());
 
         simpMessagingTemplate.convertAndSendToUser(message.getChatRoomId().toString(),"/private",message);
-        Long savedChat = chatService.saveChat(loginMember.getId(), message.getReceiverId(), message.getMessage(), false);
+        Long savedChat = chatService.saveChat(message.getMessage(), loginMember.getId(), message.getReceiverId());
 
         emitterService.notify(message.getReceiverId(), CHAT_MESSAGE);
         alarmService.saveAlarm(CHAT_MESSAGE, false, AlarmType.TALK, message.getReceiverId());
