7a3e67ac4c152e4c19c2f9375aeffe4d7a5defe9
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-23 16:13:14 +0900

    Test: 사용자 ID를 이용해 채팅방 조회 테스트 개발

--- src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
@@ -1,11 +1,16 @@
 package community.independe.repository;
 
+import community.independe.domain.chat.Chat;
 import community.independe.domain.chat.ChatRoom;
+import community.independe.domain.member.Member;
+import community.independe.repository.chat.ChatRepository;
 import community.independe.repository.chat.ChatRoomRepository;
 import community.independe.util.SortedStringEditor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,6 +19,10 @@ public class ChatRoomRepositoryTest {
 
     @Autowired
     private ChatRoomRepository chatRoomRepository;
+    @Autowired
+    private ChatRepository chatRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void saveTest() {
@@ -44,5 +53,38 @@ public class ChatRoomRepositoryTest {
 
         // then
         assertThat(findChatRoom.getSenderAndReceiver()).isEqualTo(senderAndReceiver);
+    }
+
+    @Test
+    void findChatRoomsByMemberIdTest() {
+        // given
+        Member member = Member.builder().build();
+        Member savedMember = memberRepository.save(member);
+        Member firstReceiver = Member.builder().build();
+        Member savedFirstReceiver = memberRepository.save(firstReceiver);
+        Member secondReceiver = Member.builder().build();
+        Member savedSecondReceiver = memberRepository.save(secondReceiver);
+
+        String firstSendAndReceive = SortedStringEditor.createSortedString(savedMember.getId(), savedFirstReceiver.getId());
+        ChatRoom firstChatRoom = ChatRoom.builder().senderAndReceiver(firstSendAndReceive).build();
+        ChatRoom savedFirstChatRoom = chatRoomRepository.save(firstChatRoom);
+
+        String secondSendAndReceive = SortedStringEditor.createSortedString(savedMember.getId(), savedSecondReceiver.getId());
+        ChatRoom secondChatRoom = ChatRoom.builder().senderAndReceiver(secondSendAndReceive).build();
+        ChatRoom savedSecondChatRoom = chatRoomRepository.save(secondChatRoom);
+
+        Chat firstChat = Chat.builder().sender(savedMember).receiver(savedFirstReceiver).chatRoom(savedFirstChatRoom).build();
+        Chat savedFirstChat = chatRepository.save(firstChat);
+
+        Chat secondChat = Chat.builder().sender(savedMember).receiver(savedSecondReceiver).chatRoom(savedSecondChatRoom).build();
+        Chat savedSecondChat = chatRepository.save(secondChat);
+
+        // when
+        List<ChatRoom> findChatRooms = chatRoomRepository.findChatRoomsByMemberId(member.getId());
+
+        // then
+        assertThat(findChatRooms.size()).isEqualTo(2);
+        assertThat(findChatRooms.get(0).getId()).isEqualTo(savedFirstChatRoom.getId());
+        assertThat(findChatRooms.get(1).getId()).isEqualTo(savedSecondChatRoom.getId());
     }
 }
