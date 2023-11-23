032cd406c43b6d6f1a66f42d582911e210105d5e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-23 14:33:58 +0900

    Test: senderAndReceiver 로 채팅방 조회 테스트 개발

--- src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatRoomRepositoryTest.java
@@ -2,6 +2,7 @@ package community.independe.repository;
 
 import community.independe.domain.chat.ChatRoom;
 import community.independe.repository.chat.ChatRoomRepository;
+import community.independe.util.SortedStringEditor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,5 +28,21 @@ public class ChatRoomRepositoryTest {
         assertThat(savedChatRoom).isEqualTo(chatRoom);
         assertThat(savedChatRoom.getId()).isEqualTo(chatRoom.getId());
         assertThat(savedChatRoom.getSenderAndReceiver()).isEqualTo(chatRoom.getSenderAndReceiver());
+    }
+
+    @Test
+    void findBySenderAndReceiverTest() {
+        // given
+        Long senderId = 1L;
+        Long receiverId = 2L;
+        String senderAndReceiver = SortedStringEditor.createSortedString(senderId, receiverId);
+        ChatRoom chatRoom = ChatRoom.builder().senderAndReceiver(senderAndReceiver).build();
+        chatRoomRepository.save(chatRoom);
+
+        // when
+        ChatRoom findChatRoom = chatRoomRepository.findBySenderAndReceiver(senderAndReceiver);
+
+        // then
+        assertThat(findChatRoom.getSenderAndReceiver()).isEqualTo(senderAndReceiver);
     }
 }
