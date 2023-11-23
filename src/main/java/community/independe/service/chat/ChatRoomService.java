6659618403d433debbffeb4c7777bda3ae183a29
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-23 17:56:12 +0900

    Feat: 채팅방 목록 조회 서비스 로직 개발

--- src/main/java/community/independe/service/chat/ChatRoomService.java
+++ src/main/java/community/independe/service/chat/ChatRoomService.java
@@ -1,9 +1,13 @@
 package community.independe.service.chat;
 
 import community.independe.api.dtos.chat.ChatRoomResponse;
+import community.independe.api.dtos.chat.ChatRoomsResponse;
+
+import java.util.List;
 
 public interface ChatRoomService {
 
     Long saveChatRoom(Long senderId, Long receiverId);
     ChatRoomResponse findBySenderAndReceiver(Long senderId, Long receiverId);
+    List<ChatRoomsResponse> findChatRooms(Long memberId);
 }
--- src/main/java/community/independe/service/chat/ChatRoomServiceImpl.java
+++ src/main/java/community/independe/service/chat/ChatRoomServiceImpl.java
@@ -1,17 +1,23 @@
 package community.independe.service.chat;
 
 import community.independe.api.dtos.chat.ChatRoomResponse;
+import community.independe.api.dtos.chat.ChatRoomsResponse;
+import community.independe.domain.chat.Chat;
 import community.independe.domain.chat.ChatRoom;
 import community.independe.domain.member.Member;
 import community.independe.exception.CustomException;
 import community.independe.exception.ErrorCode;
 import community.independe.repository.MemberRepository;
+import community.independe.repository.chat.ChatRepository;
 import community.independe.repository.chat.ChatRoomRepository;
 import community.independe.util.SortedStringEditor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -20,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatRoomServiceImpl implements ChatRoomService{
 
     private final ChatRoomRepository chatRoomRepository;
+    private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
 
 
@@ -58,5 +65,26 @@ public class ChatRoomServiceImpl implements ChatRoomService{
         return ChatRoomResponse.builder()
                 .chatRoomId(findChatRoom.getId())
                 .build();
+    }
+
+    @Override
+    public List<ChatRoomsResponse> findChatRooms(Long memberId) {
+        List<ChatRoom> findChatRooms = chatRoomRepository.findChatRoomsByMemberId(memberId);
+
+        List<ChatRoomsResponse> chatRoomsResponses = new ArrayList<>();
+
+        for (ChatRoom findChatRoom : findChatRooms) {
+            Chat findLastChat = chatRepository.findLastChatByChatRoomId(findChatRoom.getId());
+            ChatRoomsResponse chatRoomsResponse = ChatRoomsResponse.builder()
+                    .chatRoomId(findChatRoom.getId())
+                    .senderNickname(findLastChat.getSender().getNickname())
+                    .receiverNickname(findLastChat.getReceiver().getNickname())
+                    .isRead(findLastChat.getIsRead())
+                    .lastMessage(findLastChat.getMessage())
+                    .build();
+            chatRoomsResponses.add(chatRoomsResponse);
+        }
+
+        return chatRoomsResponses;
     }
 }
