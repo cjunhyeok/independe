5ab84f26537e5ed3ccc4c464175c0e0589b81621
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-24 15:08:45 +0900

    Feat: 채팅방 오류 중복 삭제

--- src/main/java/community/independe/exception/ErrorCode.java
+++ src/main/java/community/independe/exception/ErrorCode.java
@@ -16,12 +16,12 @@ public enum ErrorCode {
     REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 refresh token 입니다."),
     REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "잘못된 refresh token 요청 입니다."),
     REFRESH_IP_NOT_MATCH(HttpStatus.BAD_REQUEST, "저장된 토큰의 IP와 일치하지 않습니다."),
+    CHAT_ROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, "채팅방이 존재하지 않습니다."),
     FAVORITE_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "즐겨찾기 정보가 존재하지 않습니다."),
     RECOMMEND_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "게시글 추천 정보가 존재하지 않습니다."),
     COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "댓글이 존재하지 않습니다"),
     POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "게시글이 존재하지 않습니다."),
     FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "파일이 존재하지 않습니다."),
-    CHAT_ROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, "채팅방이 존재하지 않습니다."),
     REGION_NOT_AUTHENTICATE(HttpStatus.BAD_REQUEST, "위치 인증이 완료되지 않았습니다."),
     USERNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "중복된 ID 입니다."),
     NICKNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "중복된 닉네임 입니다.");
