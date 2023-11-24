2bea97af6b99ccbf97c6078fa0243b212e4f0141
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-24 15:08:32 +0900

    Test: 채팅방 관련 테스트 추가 및 실패 테스트 개발

--- src/test/java/community/independe/service/ChatServiceTest.java
+++ src/test/java/community/independe/service/ChatServiceTest.java
@@ -1,10 +1,15 @@
 package community.independe.service;
 
 import community.independe.domain.chat.Chat;
+import community.independe.domain.chat.ChatRoom;
 import community.independe.domain.member.Member;
+import community.independe.exception.CustomException;
+import community.independe.exception.ErrorCode;
 import community.independe.repository.MemberRepository;
 import community.independe.repository.chat.ChatRepository;
+import community.independe.repository.chat.ChatRoomRepository;
 import community.independe.service.chat.ChatServiceImpl;
+import org.assertj.core.api.AbstractObjectAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -25,6 +31,8 @@ public class ChatServiceTest {
     private ChatRepository chatRepository;
     @Mock
     private MemberRepository memberRepository;
+    @Mock
+    private ChatRoomRepository chatRoomRepository;
 
     @Test
     void saveChatTest() {
@@ -32,20 +40,105 @@ public class ChatServiceTest {
         String message = "message";
         Long senderId = 1L;
         Long receiverId = 2L;
+        Long chatRoomId = 1L;
         Member sender = Member.builder().build();
         Member receiver = Member.builder().build();
+        ChatRoom chatRoom = ChatRoom.builder().build();
 
         // stub
         when(memberRepository.findById(senderId)).thenReturn(Optional.of(sender));
         when(memberRepository.findById(receiverId)).thenReturn(Optional.of(receiver));
+        when(chatRoomRepository.findById(chatRoomId)).thenReturn(Optional.of(chatRoom));
         when(chatRepository.save(any(Chat.class))).thenReturn(Chat.builder().build());
 
         // when
-        chatService.saveChat(message, senderId, receiverId);
+        chatService.saveChat(message, senderId, receiverId, chatRoomId);
 
         // then
         verify(memberRepository, times(1)).findById(senderId);
         verify(memberRepository, times(1)).findById(receiverId);
+        verify(chatRoomRepository, times(1)).findById(chatRoomId);
         verify(chatRepository, times(1)).save(any(Chat.class));
+    }
+
+    @Test
+    void saveChatSenderFailTest() {
+        // given
+        String message = "message";
+        Long senderId = 1L;
+        Long receiverId = 2L;
+        Long chatRoomId = 1L;
+
+        // stub
+        when(memberRepository.findById(senderId)).thenReturn(Optional.empty());
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> chatService.saveChat(message, senderId, receiverId, chatRoomId))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.MEMBER_NOT_FOUND);
+        });
+        verify(memberRepository, times(1)).findById(senderId);
+        verifyNoMoreInteractions(memberRepository);
+        verifyNoInteractions(chatRoomRepository);
+        verifyNoInteractions(chatRepository);
+    }
+
+    @Test
+    void saveChatReceiverFailTest() {
+        // given
+        String message = "message";
+        Long senderId = 1L;
+        Long receiverId = 2L;
+        Long chatRoomId = 1L;
+
+        // stub
+        when(memberRepository.findById(senderId)).thenReturn(Optional.of(Member.builder().build()));
+        when(memberRepository.findById(receiverId)).thenReturn(Optional.empty());
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> chatService.saveChat(message, senderId, receiverId, chatRoomId))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.MEMBER_NOT_FOUND);
+        });
+        verify(memberRepository, times(1)).findById(senderId);
+        verify(memberRepository, times(1)).findById(receiverId);
+        verifyNoInteractions(chatRoomRepository);
+        verifyNoInteractions(chatRepository);
+    }
+
+    @Test
+    void saveChatChatRoomFailTest() {
+        // given
+        String message = "message";
+        Long senderId = 1L;
+        Long receiverId = 2L;
+        Long chatRoomId = 1L;
+
+        // stub
+        when(memberRepository.findById(senderId)).thenReturn(Optional.of(Member.builder().build()));
+        when(memberRepository.findById(receiverId)).thenReturn(Optional.of(Member.builder().build()));
+        when(chatRoomRepository.findById(chatRoomId)).thenReturn(Optional.empty());
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> chatService.saveChat(message, senderId, receiverId, chatRoomId))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.CHAT_ROOM_NOT_FOUND);
+        });
+        verify(memberRepository, times(1)).findById(senderId);
+        verify(memberRepository, times(1)).findById(receiverId);
+        verify(chatRoomRepository, times(1)).findById(chatRoomId);
+        verifyNoInteractions(chatRepository);
     }
 }
