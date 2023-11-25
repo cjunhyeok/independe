b1474f2aae2e8ea3b0c3bed6f8dbceedc5aaec67
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-25 16:06:18 +0900

    Feat: 서비스 레이어 채팅 내역 조회 개발

--- src/main/java/community/independe/service/chat/ChatService.java
+++ src/main/java/community/independe/service/chat/ChatService.java
@@ -1,6 +1,11 @@
 package community.independe.service.chat;
 
+import community.independe.api.dtos.chat.ChatHistoryResponse;
+
+import java.util.List;
+
 public interface ChatService {
 
     Long saveChat(String message, Long senderId, Long receiverId, Long chatRoomId);
+    List<ChatHistoryResponse> findChatHistory(Long chatRoomId);
 }
