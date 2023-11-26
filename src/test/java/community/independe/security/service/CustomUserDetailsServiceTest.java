cde75402396c6fd9a7b7f33b02d6d108bc34a02e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-26 17:52:48 +0900

    Test: memberContextTest 개발

--- src/test/java/community/independe/security/service/CustomUserDetailsServiceTest.java
+++ src/test/java/community/independe/security/service/CustomUserDetailsServiceTest.java
@@ -10,7 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -63,5 +70,28 @@ public class CustomUserDetailsServiceTest {
         extracting.satisfies(ex -> {
             assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.INVALID_USERNAME);
         });
+    }
+
+    @Test
+    void memberContextTest() {
+        // given
+        String username = "username";
+        String role = "ROLE_USER";
+        String password = "password";
+        Member member = Member.builder().username(username).password(password).role(role).build();
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(member.getRole()));
+        Map<String, Object> attributes = new HashMap<>();
+
+        // when
+        MemberContext memberContext = new MemberContext(member, roles, attributes);
+
+        // then
+        assertThat(memberContext.getMember()).isEqualTo(member);
+        assertThat(memberContext.getPassword()).isEqualTo(member.getPassword());
+        assertThat(memberContext.getAttributes()).isEqualTo(attributes);
+        assertThat(memberContext.getName()).isNull();
+
+
     }
 }
