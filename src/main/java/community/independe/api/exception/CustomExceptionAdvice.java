faea692b045c7ff6e3c659c4ba415b0b314e47bf
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-12-23 15:49:34 +0900

    Feat: Valid 예외 및 서버 예외처리 로직 추가

--- src/main/java/community/independe/api/exception/CustomExceptionAdvice.java
+++ src/main/java/community/independe/api/exception/CustomExceptionAdvice.java
@@ -2,7 +2,11 @@ package community.independe.api.exception;
 
 import community.independe.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +18,31 @@ public class CustomExceptionAdvice {
     public ResponseEntity<ErrorResult> customExceptionHandler(CustomException e) {
         ErrorResult errorResult = new ErrorResult(e.getErrorCode().getStatus(), e.getErrorCode().getErrorMessage());
         return new ResponseEntity<>(errorResult, e.getErrorCode().getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResult> processValidationError(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        ErrorResult errorResult = new ErrorResult(HttpStatus.BAD_REQUEST, builder.toString());
+
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResult> exceptionHandler(Exception e) {
+        ErrorResult errorResult = new ErrorResult(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
