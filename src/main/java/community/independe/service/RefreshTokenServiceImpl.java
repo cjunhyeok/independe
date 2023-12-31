08277e19e3d12cafdb34413a17bcfa35ccc8b155
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-12-31 14:06:12 +0900

    Feat: redis 키 구분을 위한 prefix 추가 및 ttl 설정

--- src/main/java/community/independe/service/RefreshTokenServiceImpl.java
+++ src/main/java/community/independe/service/RefreshTokenServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -24,15 +25,16 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     private final SecuritySigner securitySigner;
     private final JWK jwk;
     private final RedisTemplate redisTemplate;
+    private final String RedisKeyPrefix = "refreshToken : ";
 
     @Override
     @Transactional
     public String save(String ip, String role, String refreshToken, String username) {
 
-        String findRefreshToken = (String) redisTemplate.opsForHash().get(username, "refreshToken");
+        String findRefreshToken = (String) redisTemplate.opsForHash().get(RedisKeyPrefix + username, "refreshToken");
 
         if (findRefreshToken != null) {
-            redisTemplate.delete(username);
+            redisTemplate.delete(RedisKeyPrefix + username);
         }
 
         Map<String, String> stringStringMap = saveRefreshToken(ip, role, refreshToken, username);
@@ -46,17 +48,17 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
         String bearerToken = refreshToken.replace("; Secure; HttpOnly", "");
         jwtTokenVerifier.verifyToken(bearerToken);
 
-        String findRefreshToken = (String) redisTemplate.opsForHash().get(username, "refreshToken");
+        String findRefreshToken = (String) redisTemplate.opsForHash().get(RedisKeyPrefix + username, "refreshToken");
         if (findRefreshToken == null) {
             throw new CustomException(ErrorCode.REFRESH_TOKEN_NOT_MATCH);
         }
 
-        String savedIp = (String) redisTemplate.opsForHash().get(username, "ip");
+        String savedIp = (String) redisTemplate.opsForHash().get(RedisKeyPrefix + username, "ip");
         if (!currentIp.equals(savedIp)) {
             throw new CustomException(ErrorCode.REFRESH_IP_NOT_MATCH);
         }
 
-        String role = (String) redisTemplate.opsForHash().get(username, "role");
+        String role = (String) redisTemplate.opsForHash().get(RedisKeyPrefix + username, "role");
         String newRefreshToken = securitySigner.getRefreshJwtToken(username, jwk);
         saveRefreshToken(currentIp, role, newRefreshToken, username);
 
@@ -66,7 +68,8 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
     private Map<String, String> saveRefreshToken(String currentIp, String role, String refreshToken, String username) {
 
         Map<String, String> refreshTokenMap = RefreshTokenMapper.refreshTokenMap(refreshToken, username, role, currentIp);
-        redisTemplate.opsForHash().putAll(username, refreshTokenMap);
+        redisTemplate.opsForHash().putAll(RedisKeyPrefix + username, refreshTokenMap);
+        redisTemplate.expire(RedisKeyPrefix + username, 7L, TimeUnit.DAYS);
         return refreshTokenMap;
     }
 }
