1b60f7c58503a9d39981299bf1c3ea559f269c11
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-02 21:57:20 +0900

    Test: Redis 세션 전용 테스트 개발

---
+++ src/test/java/community/independe/repository/RedisSessionTest.java
@@ -0,0 +1,84 @@
+package community.independe.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RedisSessionTest {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @AfterEach
+    public void deleteData() {
+        redisTemplate.delete("key");
+    }
+
+    @Test
+    void setAddTest() {
+        // given
+        String key = "key";
+        String firstValue = "firstValue";
+        String secondValue = "secondValue";
+
+        // when
+        redisTemplate.opsForSet().add(key, firstValue, secondValue);
+
+        // then
+        Set<String> values = redisTemplate.opsForSet().members(key);
+        assertThat(values.size()).isEqualTo(2);
+    }
+
+    @Test
+    void setRemoveTest() {
+        // given
+        String key = "key";
+        String value = "value";
+        redisTemplate.opsForSet().add(key, value);
+
+        // when
+        redisTemplate.opsForSet().remove(key, value);
+
+        // then
+        Set<String> members = redisTemplate.opsForSet().members(key);
+        assertThat(members.isEmpty()).isTrue();
+    }
+
+    @Test
+    void stringSetTest() {
+        // given
+        String key = "key";
+        String value = "value";
+
+        // when
+        redisTemplate.opsForValue().set(key, value);
+
+        // then
+        String findValue = redisTemplate.opsForValue().get(key);
+        assertThat(findValue).isEqualTo(value);
+    }
+
+    @Test
+    void stringDeleteTest() {
+        // given
+        String key = "key";
+        String value = "value";
+        redisTemplate.opsForValue().set(key, value);
+
+        // when
+        redisTemplate.opsForValue().getOperations().delete(key);
+
+        // then
+        String findValue = redisTemplate.opsForValue().get(key);
+        assertThat(findValue).isNull();
+    }
+}
