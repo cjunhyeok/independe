a9967f2c93604a33e39f8bc17beabe7f118b1a1b
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-07 23:07:34 +0900

    Refactor: 예외 메시지 구조 변경으로 사용하지 않는 필드 삭

--- src/main/java/community/independe/api/dtos/chat/Message.java
+++ src/main/java/community/independe/api/dtos/chat/Message.java
@@ -17,7 +17,6 @@ public class Message {
     private String senderNickname;
     private LocalDateTime createdDate;
     private Boolean isRead;
-    private Boolean isExceptionData;
 
     @Builder
     public Message(String message, Long chatId, Long receiverId, Long chatRoomId, String senderNickname, LocalDateTime createdDate) {
@@ -28,6 +27,5 @@ public class Message {
         this.senderNickname = senderNickname;
         this.createdDate = createdDate;
         this.isRead = false;
-        this.isExceptionData = false;
     }
 }
