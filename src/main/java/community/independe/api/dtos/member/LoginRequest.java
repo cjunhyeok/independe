05a115d133163536549367958fd91255d8391f22
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-05 23:55:00 +0900

    Refactor: LoginRequest 내부 서비스 전용 DTO 생성 메서드 개발

--- src/main/java/community/independe/api/dtos/member/LoginRequest.java
+++ src/main/java/community/independe/api/dtos/member/LoginRequest.java
@@ -1,5 +1,6 @@
 package community.independe.api.dtos.member;
 
+import community.independe.service.dtos.LoginServiceDto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -16,4 +17,13 @@ public class LoginRequest {
     @Size(min = 8)
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$")
     private String password;
+
+    public static LoginServiceDto loginRequestToLoginServiceDto (LoginRequest request, String ip) {
+        return LoginServiceDto
+                .builder()
+                .username(request.username)
+                .password(request.getPassword())
+                .ip(ip)
+                .build();
+    }
 }
