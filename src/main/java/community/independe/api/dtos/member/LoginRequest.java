de790229625b2dbd914d9e9425de71c0dd5b08eb
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-03-28 19:41:29 +0900

    Feat: 스웨거로 로그인 시 간편화를 위해 초기 계정 설정

--- src/main/java/community/independe/api/dtos/member/LoginRequest.java
+++ src/main/java/community/independe/api/dtos/member/LoginRequest.java
@@ -1,6 +1,7 @@
 package community.independe.api.dtos.member;
 
 import community.independe.service.dtos.LoginServiceDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 public class LoginRequest {
 
     @NotEmpty
+    @Schema(description = "회원 ID", example = "id1")
     private String username;
     @NotEmpty
     @Size(min = 8)
+    @Schema(description = "비밀번호", example = "abc12!")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$")
     private String password;
 
