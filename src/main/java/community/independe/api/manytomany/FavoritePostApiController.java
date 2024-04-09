1344520aec415dc1a3b6b328ffbe31473e57e05f
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-04-09 21:10:32 +0900

    Feat: 즐겨찾기 목록 API 개발

--- src/main/java/community/independe/api/manytomany/FavoritePostApiController.java
+++ src/main/java/community/independe/api/manytomany/FavoritePostApiController.java
@@ -1,17 +1,24 @@
 package community.independe.api.manytomany;
 
+import community.independe.api.dtos.Result;
+import community.independe.api.dtos.manytomany.recommendpost.FavoritePostResponse;
 import community.independe.domain.manytomany.FavoritePost;
 import community.independe.domain.member.Member;
 import community.independe.security.service.MemberContext;
 import community.independe.service.manytomany.FavoritePostService;
+import community.independe.service.manytomany.dtos.GetFavoritePostServiceDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -36,6 +43,27 @@ public class FavoritePostApiController {
             favoritePostService.updateIsFavorite(findFavoritePost, false);
         }
         return ResponseEntity.ok("OK");
+    }
+
+    // 즐겨찾기 목록
+    @GetMapping("/api/favoritePost")
+    @Operation(summary = "즐겨찾기 목록 조회 * (마이페이지)")
+    public Result<List<FavoritePostResponse>> getFavoritePost(@AuthenticationPrincipal MemberContext memberContext) {
+        Member loginMember = memberContext.getMember();
+
+        List<GetFavoritePostServiceDto> serviceDto = favoritePostService.findFavoritePostByMemberId(loginMember.getId());
+        List<FavoritePostResponse> response = serviceDto.stream()
+                .map(sd -> FavoritePostResponse.builder()
+                            .title(sd.getTitle())
+                            .independentPostType(sd.getIndependentPostType())
+                            .regionType(sd.getRegionType())
+                            .regionPostType(sd.getRegionPostType())
+                            .nickname(sd.getNickname())
+                            .createdDate(sd.getCreatedDate())
+                            .build()
+                ).collect(Collectors.toList());
+
+        return new Result<>(response);
     }
 
 }
