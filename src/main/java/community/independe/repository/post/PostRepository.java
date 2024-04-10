df56ecc0f437856360da08c22b9dde04e5f43944
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-04-10 19:11:20 +0900

    Feat: 회원 PK로 작성한 게시글을 조회하는 쿼리 개발

--- src/main/java/community/independe/repository/post/PostRepository.java
+++ src/main/java/community/independe/repository/post/PostRepository.java
@@ -2,6 +2,14 @@ package community.independe.repository.post;
 
 import community.independe.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+
+    @Query(value = "select p from Post p" +
+            " where p.member.id = :memberId")
+    List<Post> findAllByMemberId(@Param("memberId") Long memberId);
 }
