f9e6dbb91deb714e01d56645a8d400788bce6dd2
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-04-13 16:31:05 +0900

    Feat: 작성한 게시글 조회 / 추천한 게시글 조회 개발 및 페이징 적용

--- src/main/java/community/independe/repository/post/PostRepository.java
+++ src/main/java/community/independe/repository/post/PostRepository.java
@@ -1,16 +1,24 @@
 package community.independe.repository.post;
 
 import community.independe.domain.post.Post;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
     @Query(value = "select p from Post p" +
             " where p.member.id = :memberId")
-    List<Post> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+    // 작성한 게시글 조회
+    Page<Post> findAllByMemberId(@Param("memberId") Long memberId,
+                                 Pageable pageable);
+
+    @Query(value = "select rp.post from RecommendPost rp" +
+            " where rp.member.id = :memberId" +
+            " and rp.isRecommend = true")
+    // 추천한 게시글 조회
+    Page<Post> findRecommendPostByMemberId(@Param("memberId") Long memberId,
+                                           Pageable pageable);
 }
