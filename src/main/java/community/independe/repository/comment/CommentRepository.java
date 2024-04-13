fe3f6e7e42ca2f22745ebb7b3c9c98aad516601d
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-04-13 16:28:50 +0900

    Feat: 작성한 댓글 조회 / 추천한 댓글 조회 쿼리 개발

--- src/main/java/community/independe/repository/comment/CommentRepository.java
+++ src/main/java/community/independe/repository/comment/CommentRepository.java
@@ -1,6 +1,8 @@
 package community.independe.repository.comment;
 
 import community.independe.domain.comment.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,9 +21,16 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
 
     Long countAllByPostId(Long PostId);
 
-    @Query(value = "select c from Comment c left join fetch c.parent" +
-            " join fetch c.member" +
-            " join fetch c.post" +
+    @Query(value = "select c from Comment c" +
             " where c.member.id = :memberId")
-    List<Comment> findAllByMemberId(@Param("memberId") Long memberId);
+    // 작성한 댓글 조회
+    Page<Comment> findAllByMemberId(@Param("memberId") Long memberId,
+                                    Pageable pageable);
+
+    @Query(value = "select rc.comment from RecommendComment rc" +
+            " where rc.member.id = :memberId" +
+            " and rc.isRecommend = true")
+    // 추천한 댓글 조회
+    Page<Comment> findRecommendCommentByMemberId(@Param("memberId") Long memberId,
+                                                 Pageable pageable);
 }
