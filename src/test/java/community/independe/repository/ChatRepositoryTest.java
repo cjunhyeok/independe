8b48e9f33cb60be044b5898420ba17a35119a335
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-01 18:56:53 +0900

    Test: 채팅을 회원, 채팅방과 함께 저장하는 테스트 개발

--- src/test/java/community/independe/repository/ChatRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatRepositoryTest.java
@@ -6,12 +6,10 @@ import community.independe.domain.chat.ChatRoom;
 import community.independe.domain.member.Member;
 import community.independe.repository.chat.ChatRepository;
 import community.independe.repository.chat.ChatRoomRepository;
-import community.independe.util.SortedStringEditor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -26,8 +24,20 @@ public class ChatRepositoryTest extends IntegrationTestSupporter {
     private ChatRoomRepository chatRoomRepository;
 
     @Test
+    @DisplayName("채팅을 회원, 채팅방과 함께 저장한다.")
     void saveTest() {
         // given
-        Member sender = Member.builder().build();
+        Member member = Member.builder().build();
+        Member savedMember = memberRepository.save(member);
+        ChatRoom chatRoom = ChatRoom.builder().title("title").build();
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        Chat chat = Chat.builder().message("message").chatRoom(savedChatRoom).member(savedMember).build();
+
+        // when
+        Chat savedChat = chatRepository.save(chat);
+
+        // then
+        assertThat(savedChat.getMember()).isEqualTo(savedMember);
+        assertThat(savedChat.getChatRoom()).isEqualTo(savedChatRoom);
     }
 }
