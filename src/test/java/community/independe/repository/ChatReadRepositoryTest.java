1a3e0ddf9ee58e2885f96f2e64fa501ac2ab714f
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-04 21:29:58 +0900

    Test: 회원 PK 와 채팅방 PK를 이용해 읽지않은 메시지 개수를 조회 테스트 개발

--- src/test/java/community/independe/repository/ChatReadRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatReadRepositoryTest.java
@@ -50,4 +50,42 @@ public class ChatReadRepositoryTest extends IntegrationTestSupporter {
         assertThat(savedChatRead.getMember()).isEqualTo(savedReceiver);
         assertThat(savedChatRead.getIsRead()).isFalse();
     }
+
+    @Test
+    @DisplayName("회원 PK 와 채팅방 PK를 이용해 읽지않은 메시지 개수를 조회한다.")
+    void findUnReadCountByChatIdAndMemberIdTest() {
+        // given
+        Member sender = Member.builder().username("sender").password("pass").nickname("sender").build();
+        Member savedSender = memberRepository.save(sender);
+        Member receiver = Member.builder().username("receiver").password("pass").nickname("receiver").build();
+        Member savedReceiver = memberRepository.save(receiver);
+        ChatRoom chatRoom = ChatRoom.builder().title("title").build();
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        Chat chat = Chat.builder().message("message").chatRoom(savedChatRoom).member(savedSender).build();
+        Chat savedChat = chatRepository.save(chat);
+        Chat secondChat = Chat.builder().message("message").chatRoom(savedChatRoom).member(savedSender).build();
+        Chat savedSecondChat = chatRepository.save(secondChat);
+        Chat thirdChat = Chat.builder().message("message").chatRoom(savedChatRoom).member(savedSender).build();
+        Chat savedThirdChat = chatRepository.save(thirdChat);
+
+        ChatRead chatRead = ChatRead.builder().isRead(true).chat(savedChat).member(savedReceiver).build();
+        chatReadRepository.save(chatRead);
+        ChatRead chatSenderRead = ChatRead.builder().isRead(true).chat(savedChat).member(savedSender).build();
+        chatReadRepository.save(chatSenderRead);
+        ChatRead secondChatRead = ChatRead.builder().isRead(false).chat(savedSecondChat).member(savedReceiver).build();
+        chatReadRepository.save(secondChatRead);
+        ChatRead secondChatSenderRead = ChatRead.builder().isRead(true).chat(savedSecondChat).member(savedSender).build();
+        chatReadRepository.save(secondChatSenderRead);
+        ChatRead thirdChatRead = ChatRead.builder().isRead(false).chat(savedThirdChat).member(savedReceiver).build();
+        chatReadRepository.save(thirdChatRead);
+        ChatRead thirdChatSenderRead = ChatRead.builder().isRead(true).chat(savedThirdChat).member(savedSender).build();
+        chatReadRepository.save(thirdChatSenderRead);
+
+        // when
+        Long findUnReadCount
+                = chatReadRepository.findUnReadCountByChatRoomIdAndMemberId(savedChatRoom.getId(), savedReceiver.getId());
+
+        // then
+        assertThat(findUnReadCount).isEqualTo(2);
+    }
 }
