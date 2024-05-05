2893536475d3c8a1830c573243834b0a788155f0
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-05 15:44:43 +0900

    Refactor: 채팅 구조 개편으로 인채 채팅 저장, 채팅 내역 조회 서비스 코드 리팩토링 및 PK 로 채팅 정보 조회 개발

--- src/main/java/community/independe/service/chat/ChatService.java
+++ src/main/java/community/independe/service/chat/ChatService.java
@@ -1,12 +1,14 @@
 package community.independe.service.chat;
 
 import community.independe.api.dtos.chat.ChatHistoryResponse;
+import community.independe.domain.chat.Chat;
+import community.independe.service.chat.dtos.SaveChatDto;
 
 import java.util.List;
 
 public interface ChatService {
 
-    Long saveChat(String message, Long senderId, Long receiverId, Long chatRoomId, Boolean isRead);
+    Long saveChat(SaveChatDto dto);
     List<ChatHistoryResponse> findChatHistory(Long chatRoomId, Long memberId);
-    void updateChatIsRead(Long chatId, Long chatRoomId, Long memberId);
+    Chat findById(Long chatId);
 }
--- src/main/java/community/independe/service/chat/ChatServiceImpl.java
+++ src/main/java/community/independe/service/chat/ChatServiceImpl.java
@@ -2,13 +2,16 @@ package community.independe.service.chat;
 
 import community.independe.api.dtos.chat.ChatHistoryResponse;
 import community.independe.domain.chat.Chat;
+import community.independe.domain.chat.ChatRead;
 import community.independe.domain.chat.ChatRoom;
 import community.independe.domain.member.Member;
 import community.independe.exception.CustomException;
 import community.independe.exception.ErrorCode;
 import community.independe.repository.MemberRepository;
+import community.independe.repository.chat.ChatReadRepository;
 import community.independe.repository.chat.ChatRepository;
 import community.independe.repository.chat.ChatRoomRepository;
+import community.independe.service.chat.dtos.SaveChatDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,86 +29,70 @@ public class ChatServiceImpl implements ChatService {
     private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
     private final ChatRoomRepository chatRoomRepository;
+    private final ChatReadRepository chatReadRepository;
 
     @Override
     @Transactional
-    public Long saveChat(String message, Long senderId, Long receiverId, Long chatRoomId, Boolean isRead) {
-//        Member findSender = memberRepository.findById(senderId).orElseThrow(
-//                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
-//        );
-//
-//        Member findReceiver = memberRepository.findById(receiverId).orElseThrow(
-//                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
-//        );
-//
-//        ChatRoom findChatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(
-//                () -> new CustomException(ErrorCode.CHAT_ROOM_NOT_FOUND)
-//        );
-//
-//        Chat chat = Chat.builder()
-//                .message(message)
-//                .sender(findSender)
-//                .receiver(findReceiver)
-//                .isRead(isRead)
-//                .chatRoom(findChatRoom)
-//                .build();
-//        Chat savedChat = chatRepository.save(chat);
-//
-//        return savedChat.getId();
-        return null;
+    public Long saveChat(SaveChatDto dto) {
+        Member findSender = memberRepository.findById(dto.getSenderId()).orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        ChatRoom findChatRoom = chatRoomRepository.findById(dto.getChatRoomId()).orElseThrow(
+                () -> new CustomException(ErrorCode.CHAT_ROOM_NOT_FOUND)
+        );
+
+        Chat chat = Chat.builder()
+                .message(dto.getMessage())
+                .member(findSender)
+                .chatRoom(findChatRoom)
+                .build();
+        Chat savedChat = chatRepository.save(chat);
+
+        return savedChat.getId();
     }
 
     @Override
     @Transactional
     public List<ChatHistoryResponse> findChatHistory(Long chatRoomId, Long memberId) {
-//        List<Chat> chatHistory = chatRepository.findChatHistory(chatRoomId);
-//        Member findMember = memberRepository.findById(memberId).orElseThrow(
-//                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
-//        );
-//
-//        List<ChatHistoryResponse> chatHistoryResponses = new ArrayList<>();
-//
-//        for (Chat chat : chatHistory) {
-//
-//            if (chat.getReceiver() == findMember) {
-//                chat.updateIsReadTrue();
-//            }
-//
-//            ChatHistoryResponse chatHistoryResponse = new ChatHistoryResponse();
-//            chatHistoryResponse.setChatId(chat.getId());
-//            chatHistoryResponse.setSenderNickname(chat.getSender().getNickname());
-//            chatHistoryResponse.setReceiverNickname(chat.getReceiver().getNickname());
-//            chatHistoryResponse.setMessage(chat.getMessage());
-//            chatHistoryResponse.setIsRead(chat.getIsRead());
-//            chatHistoryResponse.setCreatedDate(chat.getCreatedDate());
-//            chatHistoryResponse.setSenderId(chat.getSender().getId());
-//            chatHistoryResponses.add(chatHistoryResponse);
-//        }
 
-        return null;
+        Member findMember = memberRepository.findById(memberId).orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        List<Chat> chatHistory = chatRepository.findChatHistory(chatRoomId);
+
+        List<ChatHistoryResponse> chatHistoryResponses = new ArrayList<>();
+
+        for (Chat chat : chatHistory) {
+
+            Member sender = chat.getMember();
+
+            // 읽음처리 해줘야함 (이런걸 다른 쓰레드에서 해도 괜찮을거 같다 (IN QUERY 로 조지기?)
+            // 상대방이 안읽은 메시지도 가져와야 한다.
+            ChatRead findChatRead = chatReadRepository.findByChatId(chat.getId());
+            if (sender.getId() != memberId) {
+                // 채팅이 상대방이 보낸거일 때
+                findChatRead.updateIsReadTrue();
+            }
+            ChatHistoryResponse chatHistoryResponse = ChatHistoryResponse.builder()
+                    .chatId(chat.getId())
+                    .senderNickname(sender.getNickname())
+                    .senderId(sender.getId())
+                    .isRead(findChatRead.getIsRead())
+                    .message(chat.getMessage())
+                    .createdDate(chat.getCreatedDate())
+                    .build();
+
+            chatHistoryResponses.add(chatHistoryResponse);
+        }
+
+        return chatHistoryResponses;
     }
 
     @Override
-    @Transactional
-    public void updateChatIsRead(Long chatId, Long chatRoomId, Long memberId) {
-//        Chat findChat = chatRepository.findById(chatId).orElseThrow(
-//                () -> new CustomException(ErrorCode.CHAT_NOT_FOUND)
-//        );
-//
-//        ChatRoom findChatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(
-//                () -> new CustomException(ErrorCode.CHAT_ROOM_NOT_FOUND)
-//        );
-//
-//        if (!findChat.getChatRoom().equals(findChatRoom)) {
-//            throw new CustomException(ErrorCode.CHAT_ROOM_CHAT_NOT_MATCH);
-//        }
-//
-//        Member findMember = memberRepository.findById(memberId).orElseThrow(
-//                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
-//        );
-//
-//        if (findChat.getReceiver().equals(findMember)) {
-//            findChat.updateIsReadTrue();
-//        }
+    public Chat findById(Long chatId) {
+        return chatRepository.findById(chatId).orElseThrow(
+                () -> new CustomException(ErrorCode.CHAT_NOT_FOUND));
     }
 }
