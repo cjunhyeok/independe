4163e5e5335053e37b5b842743f4768f0f78fdd7
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-05 15:49:25 +0900

    Delete: 사용하지 않는 CustomRepository 삭제

--- src/main/java/community/independe/repository/chat/ChatRoomRepository.java
+++ src/main/java/community/independe/repository/chat/ChatRoomRepository.java
@@ -2,9 +2,7 @@ package community.independe.repository.chat;
 
 import community.independe.domain.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
 }
