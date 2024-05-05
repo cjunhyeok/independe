c1d4106a5235bbadd27b78c36a15a287af2d7561
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-05-05 16:15:06 +0900

    Test: 채팅방 PK 로 채팅 내역을 조회하는 테스트 개발

--- src/test/java/community/independe/repository/ChatRepositoryTest.java
+++ src/test/java/community/independe/repository/ChatRepositoryTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @Transactional
@@ -61,5 +63,27 @@ public class ChatRepositoryTest extends IntegrationTestSupporter {
 
         // then
         assertThat(findLastChat.getMessage()).isEqualTo(lastChat.getMessage());
+    }
+
+    @Test
+    @DisplayName("채팅방 PK 로 채팅 내역을 조회한다.")
+    void findChatHistoryTest() {
+        // given
+        Member member = Member.builder().build();
+        Member savedMember = memberRepository.save(member);
+        ChatRoom chatRoom = ChatRoom.builder().title("title").build();
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        Chat chat = Chat.builder().message("message").chatRoom(savedChatRoom).member(savedMember).build();
+        Chat savedChat = chatRepository.save(chat);
+        Chat secondChat = Chat.builder().message("secondMessage").chatRoom(savedChatRoom).member(savedMember).build();
+        Chat savedSecondChat = chatRepository.save(secondChat);
+        Chat lastChat = Chat.builder().message("lastMessage").chatRoom(savedChatRoom).member(savedMember).build();
+        Chat savedLastChat = chatRepository.save(lastChat);
+
+        // when
+        List<Chat> findChatHistory = chatRepository.findChatHistory(savedChatRoom.getId());
+
+        // then
+        assertThat(findChatHistory).hasSize(3);
     }
 }
