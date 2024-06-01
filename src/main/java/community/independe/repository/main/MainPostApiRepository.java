3ea861a355f9e4598a6f12339ba03befd38f0351
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-06-01 15:18:28 +0900

    Refactor: 메인화면 조회 쿼리 페이징 시 MainPostPageRequest 를 사용하도록 변경

--- src/main/java/community/independe/repository/main/MainPostApiRepository.java
+++ src/main/java/community/independe/repository/main/MainPostApiRepository.java
@@ -1,12 +1,12 @@
-package community.independe.repository.query;
+package community.independe.repository.main;
 
 import community.independe.domain.post.Post;
 import community.independe.domain.post.enums.RegionType;
+import community.independe.service.dtos.main.MainPostPageRequest;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -15,54 +15,54 @@ public class MainPostApiRepository {
 
     private final EntityManager em;
 
-    public List<Post> findAllPopularPosts(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
+    public List<Post> findAllPopularPosts(MainPostPageRequest request) {
         return em.createQuery("select p from Post p" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                         " order by p.views DESC", Post.class)
-                .setFirstResult(first)
-                .setMaxResults(max)
-                .setParameter("yesterday", yesterday)
-                .setParameter("today", today)
+                .setFirstResult(request.getOffset())
+                .setMaxResults(request.getLimit())
+                .setParameter("yesterday", request.getDateOffset())
+                .setParameter("today", request.getDateLimit())
                 .getResultList();
     }
 
-    public List<Post> findAllIndependentPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
+    public List<Post> findAllIndependentPostByRecommendCount(MainPostPageRequest request) {
         return em.createQuery("select p from Post p left join p.recommendPosts rp" +
                 " where p.createdDate between :yesterday and :today" +
                 " and p.independentPostType is not null" +
                 " group by p.id" +
                 " order by count(rp.post) desc, p.views desc", Post.class)
-                .setParameter("yesterday", yesterday)
-                .setParameter("today", today)
-                .setFirstResult(first)
-                .setMaxResults(max)
+                .setFirstResult(request.getOffset())
+                .setMaxResults(request.getLimit())
+                .setParameter("yesterday", request.getDateOffset())
+                .setParameter("today", request.getDateLimit())
                 .getResultList();
     }
 
-    public List<Post> findAllRegionAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
+    public List<Post> findAllRegionAllPostByRecommendCount(MainPostPageRequest request) {
         return em.createQuery("select p from Post p left join p.recommendPosts rp" +
                 " where p.createdDate between :yesterday and :today" +
                 " and p.regionType = :regionType" +
                 " group by p.id" +
                 " order by count(rp.post) desc, p.views desc", Post.class)
-                .setFirstResult(first)
-                .setMaxResults(max)
-                .setParameter("yesterday", yesterday)
-                .setParameter("today", today)
+                .setFirstResult(request.getOffset())
+                .setMaxResults(request.getLimit())
+                .setParameter("yesterday", request.getDateOffset())
+                .setParameter("today", request.getDateLimit())
                 .setParameter("regionType", RegionType.ALL)
                 .getResultList();
     }
 
-    public List<Post> findRegionNotAllPostByRecommendCount(LocalDateTime yesterday, LocalDateTime today, int first, int max) {
+    public List<Post> findRegionNotAllPostByRecommendCount(MainPostPageRequest request) {
         return em.createQuery("select p from Post p left join p.recommendPosts rp" +
                 " where p.createdDate BETWEEN :yesterday AND :today" +
                 " and p.regionType <> :regionType" +
                 " group by p.id" +
                 " order by count(rp.post) desc, p.views desc", Post.class)
-                .setFirstResult(first)
-                .setMaxResults(max)
-                .setParameter("yesterday", yesterday)
-                .setParameter("today", today)
+                .setFirstResult(request.getOffset())
+                .setMaxResults(request.getLimit())
+                .setParameter("yesterday", request.getDateOffset())
+                .setParameter("today", request.getDateLimit())
                 .setParameter("regionType", RegionType.ALL)
                 .getResultList();
     }
