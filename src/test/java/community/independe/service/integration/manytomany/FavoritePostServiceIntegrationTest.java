137e9d38e0e49cf76ced8f1d1c2789e0f7fce777
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-06-22 19:56:12 +0900

    Test: 게시글 즐겨찾기 Service 통합 테스트 개발

--- src/test/java/community/independe/service/integration/manytomany/FavoritePostServiceIntegrationTest.java
+++ src/test/java/community/independe/service/integration/manytomany/FavoritePostServiceIntegrationTest.java
@@ -5,11 +5,14 @@ import community.independe.domain.manytomany.FavoritePost;
 import community.independe.domain.member.Member;
 import community.independe.domain.post.Post;
 import community.independe.domain.post.enums.IndependentPostType;
+import community.independe.exception.CustomException;
+import community.independe.exception.ErrorCode;
 import community.independe.repository.MemberRepository;
 import community.independe.repository.manytomany.FavoritePostRepository;
 import community.independe.repository.post.PostRepository;
 import community.independe.service.manytomany.FavoritePostService;
 import community.independe.service.manytomany.dtos.GetFavoritePostServiceDto;
+import org.assertj.core.api.AbstractObjectAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Transactional
 public class FavoritePostServiceIntegrationTest extends IntegrationTestSupporter {
@@ -30,6 +34,113 @@ public class FavoritePostServiceIntegrationTest extends IntegrationTestSupporter
     private FavoritePostRepository favoritePostRepository;
     @Autowired
     private FavoritePostService favoritePostService;
+
+    @Test
+    @DisplayName("게시글 즐겨찾기를 조회한다.")
+    void saveTest() {
+        // given
+        Member savedMember = createMember();
+        Post savedPost = createPost(savedMember);
+
+        // when
+        Long savedFavoritePostId
+                = favoritePostService.save(savedPost.getId(), savedMember.getId());
+
+        // then
+        FavoritePost findFavoritePost = favoritePostRepository.findById(savedFavoritePostId).get();
+        assertThat(findFavoritePost.getId()).isEqualTo(savedFavoritePostId);
+        assertThat(findFavoritePost.getPost()).isEqualTo(savedPost);
+        assertThat(findFavoritePost.getMember()).isEqualTo(savedMember);
+        assertThat(findFavoritePost.getIsFavorite()).isTrue();
+    }
+
+    @Test
+    @DisplayName("게시글 즐겨찾기 저장 시 회원 PK 를 잘못 입력하면 예외가 발생한다.")
+    void saveMemberFailTest() {
+        // given
+        Member savedMember = createMember();
+        Post savedPost = createPost(savedMember);
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> favoritePostService.save(savedPost.getId(), savedMember.getId() + 1L))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.MEMBER_NOT_FOUND);
+        });
+    }
+
+    @Test
+    @DisplayName("게시글 즐겨찾기 저장 시 게시글 PK 를 잘못 입력하면 예외가 발생한다.")
+    void savePostFailTest() {
+        // given
+        Member savedMember = createMember();
+        Post savedPost = createPost(savedMember);
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> favoritePostService.save(savedPost.getId() + 1L, savedMember.getId()))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.POST_NOT_FOUND);
+        });
+    }
+
+    @Test
+    @DisplayName("게시글 즐겨찾기 저장 후 즐겨찾기 시 즐겨찾기 여부가 false 가 된다.")
+    void saveIsFavoriteFalseTest() {
+        // given
+        Member savedMember = createMember();
+        Post savedPost = createPost(savedMember);
+        favoritePostService.save(savedPost.getId(), savedMember.getId());
+
+        // when
+        Long savedFavoritePostId = favoritePostService.save(savedPost.getId(), savedMember.getId());
+
+        // then
+        FavoritePost findFavoritePost = favoritePostRepository.findById(savedFavoritePostId).get();
+        assertThat(findFavoritePost.getIsFavorite()).isFalse();
+    }
+
+    @Test
+    @DisplayName("게시글 즐겨찾기 저장 후 즐겨찾기 시 즐겨찾기 여부가 false 이면 true 가 된다")
+    void saveIsFavoriteTrueTest() {
+        // given
+        Member savedMember = createMember();
+        Post savedPost = createPost(savedMember);
+        favoritePostService.save(savedPost.getId(), savedMember.getId());
+        favoritePostService.save(savedPost.getId(), savedMember.getId());
+
+        // when
+        Long savedFavoritePostId = favoritePostService.save(savedPost.getId(), savedMember.getId());
+
+        // then
+        FavoritePost findFavoritePost = favoritePostRepository.findById(savedFavoritePostId).get();
+        assertThat(findFavoritePost.getIsFavorite()).isTrue();
+    }
+
+    private Member createMember() {
+        Member member = Member.builder()
+                .username("id")
+                .password("pass")
+                .nickname("nick")
+                .build();
+        return memberRepository.save(member);
+    }
+
+    private Post createPost(Member member) {
+        Post post = Post.builder()
+                .title("title")
+                .content("content")
+                .independentPostType(IndependentPostType.ETC)
+                .member(member)
+                .build();
+        return postRepository.save(post);
+    }
 
     @Test
     @DisplayName("회원 PK로 즐겨찾기 목록 DTO 를 조회한다.")
