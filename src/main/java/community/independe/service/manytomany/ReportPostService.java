e7a785096acbe7e65f3552dc04e9fd814d439129
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-06-23 15:36:33 +0900

    Feat: 게시글 신고 Entity 가 반환되지 않도록 변경 / 신고 여부를 판단하는 로직 save 에 추가

--- src/main/java/community/independe/service/manytomany/ReportPostService.java
+++ src/main/java/community/independe/service/manytomany/ReportPostService.java
@@ -1,14 +1,6 @@
 package community.independe.service.manytomany;
 
-import community.independe.domain.manytomany.ReportPost;
-
 public interface ReportPostService {
 
     Long save(Long postId, Long memberId);
-
-    ReportPost findByPostIdAndMemberId(Long postId, Long memberId);
-
-    ReportPost findByPostIdAndMemberIdAndIsRecommend(Long postId, Long memberId);
-
-    void updateIsReport(ReportPost reportPost , Boolean isRecommend);
 }
