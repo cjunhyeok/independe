bd68a1ede2d69ceb50e1debfca1d87f5cebd6516
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-06-23 15:32:03 +0900

    Refactor: 댓글 추천 시 Entity 가 반환되지 않도록 구조 변경 / 추천여부 판단 로직 save 내부에 추가

--- src/main/java/community/independe/service/manytomany/RecommendCommentService.java
+++ src/main/java/community/independe/service/manytomany/RecommendCommentService.java
@@ -1,15 +1,10 @@
 package community.independe.service.manytomany;
 
 import community.independe.api.dtos.post.BestCommentDto;
-import community.independe.domain.manytomany.RecommendComment;
 
 public interface RecommendCommentService {
 
     Long save(Long commentId, Long memberId);
-
-    RecommendComment findByCommentIdAndMemberId(Long commentId, Long memberId);
-
-    void updateIsRecommend(RecommendComment recommendComment, Boolean isRecommend);
 
     BestCommentDto findBestComment();
 }
