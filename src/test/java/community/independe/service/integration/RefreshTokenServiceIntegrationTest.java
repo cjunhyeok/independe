227ca433870f396fea062ef118774e792f36d230
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-07-04 20:01:21 +0900

    Test: reProvideRefreshToken 실패 테스트 개발

--- src/test/java/community/independe/service/integration/RefreshTokenServiceIntegrationTest.java
+++ src/test/java/community/independe/service/integration/RefreshTokenServiceIntegrationTest.java
@@ -2,12 +2,15 @@ package community.independe.service.integration;
 
 import com.nimbusds.jose.JOSEException;
 import community.independe.IntegrationTestSupporter;
+import community.independe.exception.CustomException;
+import community.independe.exception.ErrorCode;
 import community.independe.security.signature.SecuritySigner;
 import community.independe.service.MemberService;
 import community.independe.service.RefreshTokenService;
 import community.independe.service.dtos.JoinServiceDto;
 import community.independe.service.dtos.LoginResponse;
 import community.independe.service.dtos.LoginServiceDto;
+import org.assertj.core.api.AbstractObjectAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,6 +73,70 @@ public class RefreshTokenServiceIntegrationTest extends IntegrationTestSupporter
 
         // then
         assertThat(refreshToken).isNotNull();
+    }
+
+    @Test
+    @DisplayName("기존에 저장된 토큰이 없다면 예외가 발생한다.")
+    void reProvideRefreshTokenFindFailTest() throws JOSEException {
+        // given
+        String username = "username";
+        String password = "password";
+        String nickname = "nickname";
+        JoinServiceDto joinDto = createJoinDto(username, password, nickname);
+        memberService.join(joinDto);
+
+        String ip = "127.0.0.1";
+        LoginServiceDto loginServiceDto = LoginServiceDto
+                .builder()
+                .username(username)
+                .password(password)
+                .ip(ip)
+                .build();
+        LoginResponse loginResponse = memberService.login(loginServiceDto);
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() ->
+                refreshTokenService
+                        .reProvideRefreshToken(username + "1", ip, "Bearer " + loginResponse.getRefreshToken()))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.REFRESH_TOKEN_NOT_MATCH);
+        });
+    }
+    
+    @Test
+    @DisplayName("기존에 저장된 토큰과 ip가 일치하지 않다면 예외가 발생한다.")
+    void reProvideRefreshTokenIpFailTest() {
+        // given
+        String username = "username";
+        String password = "password";
+        String nickname = "nickname";
+        JoinServiceDto joinDto = createJoinDto(username, password, nickname);
+        memberService.join(joinDto);
+
+        String ip = "127.0.0.1";
+        LoginServiceDto loginServiceDto = LoginServiceDto
+                .builder()
+                .username(username)
+                .password(password)
+                .ip(ip)
+                .build();
+        LoginResponse loginResponse = memberService.login(loginServiceDto);
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() ->
+                refreshTokenService
+                        .reProvideRefreshToken(username, ip + "1", "Bearer " + loginResponse.getRefreshToken()))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.REFRESH_IP_NOT_MATCH);
+        });
     }
 
     private JoinServiceDto createJoinDto(String username, String password, String nickname) {
